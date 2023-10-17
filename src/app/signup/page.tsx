046c5390a7ad222379076b376ab129c40732ac4a
Erin Hiebert <eehieb10@telus.net>
2023-10-17 15:21:34 -0700

    Created simple signin page with no functionality

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,55 @@
+import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField'
+import Grid from '@mui/material/Grid'
+import Container from '@mui/material/Container'
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import Link from '@mui/material/Link'
+
+export default function SignIn() {
+    return(
+        <Container component='main' maxWidth='xs'>
+            <Box
+                sx={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+            >
+                <Typography component='h1' variant='h5'>
+                    Sign In
+                </Typography>
+                <Box component='form'>
+                    <TextField
+                        margin='normal'
+                        fullWidth
+                        label='Email Address'
+                        name='email'
+                    />
+                    <TextField
+                        margin='normal'
+                        fullWidth
+                        label='Password'
+                        name='password'
+                    />
+                    <Button
+                        type='submit'
+                        fullWidth
+                        variant='contained'
+                        sx={{ mt: 3, mb: 2 }}
+                    >
+                        Sign In
+                    </Button>
+                    <Grid container>
+                        <Grid item>
+                            <Link href='#' variant='body2'>
+                                Already have an account?
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Box>
+        </Container>
+    )
+}
