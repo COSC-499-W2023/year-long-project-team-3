d6a1aaa60230a519f520eefea02924ee24cc398c
te-sa <teresa_saller@freenet.de>
2023-10-16 18:52:16 -0700

    Added dotenv to package JSON, getting basUrl from .env

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'cypress'
 
+require('dotenv').config()
 export default defineConfig({
     e2e: {
-        baseUrl: 'http://localhost:3000',
+        baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
         setupNodeEvents(on, config) {
             // implement node event listeners here
         },
