418cc4c807f9a5612ca306f4db15c8272859b880
ketphan02 <tuankietvn@outlook.com>
2023-10-18 16:52:53 -0700

    Add project id for e2e

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'cypress'
 require('dotenv').config()
 
 export default defineConfig({
-    projectId: process.env.CYPRESS_PROJECT_ID,
     e2e: {
+        projectId: process.env.CYPRESS_PROJECT_ID,
         baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
         setupNodeEvents(on, config) {
             // implement node event listeners here
