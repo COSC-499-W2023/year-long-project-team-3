a55d63eaa89177c3a8a578c42b9de5360f3e87fc
ketphan02 <tuankietvn@outlook.com>
2023-10-17 23:19:13 -0700

    Remove auth API test since using third party

--- cypress/e2e/api/auth.cy.ts
+++
@@ -1,8 +0,0 @@
-describe('Test Auth API', () => {
-    it('should ', () => {
-        cy.request({
-            method: 'POST',
-            url: 'http://localhost:3000/api/auth',
-        })
-    })
-})
