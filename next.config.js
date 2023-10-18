fcf62a73cc829814093a3ac40dac0ce0bf7b7f3c
ketphan02 <tuankietvn@outlook.com>
2023-10-17 17:37:46 -0700

    Add cross-env to manage environments

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const nextConfig = () => {
+    const env = {}
+
+    const nodeEnv = process.env.NODE_ENV
+    if (nodeEnv === 'test') {
+        env.DATABASE_URL = process.env.TEST_DATABASE_URL
+    } else if (nodeEnv === 'dev') {
+        env.DATABASE_URL = process.env.DEV_DATABASE_URL
+    } else if (nodeEnv === 'prod') {
+        env.DATABASE_URL = process.env.PROD_DATABASE_URL
+    }
+
+    return {
+        env,
+    }
+}
 
 module.exports = nextConfig
