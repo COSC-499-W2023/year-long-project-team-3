a154408e42ff7621ed4cfff367a6cbbbb6a4e141
ketphan02 <tuankietvn@outlook.com>
2023-10-18 16:39:21 -0700

    try using records

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'cypress'
+require('dotenv').config()
 
 export default defineConfig({
+    projectId: process.env.CYPRESS_PROJECT_ID,
     e2e: {
         baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
         setupNodeEvents(on, config) {
