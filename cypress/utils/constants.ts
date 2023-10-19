f9284fc3cc5d1a8159e9d76d0e04abb25ff0ffcd
ketphan02 <tuankietvn@outlook.com>
2023-10-19 01:10:57 -0700

    Modify wait time

--- cypress/utils/constants.ts
+++ cypress/utils/constants.ts
@@ -1,15 +1,15 @@
 export const TIMEOUT = {
     EXTRA_SHORT: 500,
-    SHORT: 1000,
-    MEDIUM: 3000,
-    LONG: 6000,
-    EXTRA_LONG: 10000,
+    SHORT: 2000,
+    MEDIUM: 5000,
+    LONG: 10000,
+    EXTRA_LONG: 20000,
 }
 
 export const DELAY = {
     EXTRA_SHORT: 500,
-    SHORT: 1000,
-    MEDIUM: 3000,
-    LONG: 6000,
-    EXTRA_LONG: 10000,
+    SHORT: 2000,
+    MEDIUM: 5000,
+    LONG: 10000,
+    EXTRA_LONG: 20000,
 }
