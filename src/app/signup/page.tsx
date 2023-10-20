8ef1db8edf510013e7ba4ea83e13b0b1af23bf3d
Erin Hiebert <eehieb10@telus.net>
2023-10-20 15:42:24 -0700

    modified signup page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -7,7 +7,7 @@ import Typography from '@mui/material/Typography'
 import Link from '@mui/material/Link'
 
 export default function SignUp() {
-    return(
+    return (
         <Container component='main' maxWidth='xs'>
             <Box
                 sx={{
@@ -21,36 +21,21 @@ export default function SignUp() {
                     Sign Up
                 </Typography>
                 <Box component='form'>
-                    <TextField
-                        margin='normal'
-                        fullWidth
-                        label='Email Address'
-                        name='email'
-                    />
-                    <TextField
-                        margin='normal'
-                        fullWidth
-                        label='Password'
-                        name='password'
-                    />
-                    <TextField
-                        margin='normal'
-                        fullWidth
-                        label='Confirm Password'
-                        name='passwordValidation'
-                    />
-                    <Button
-                        type='submit'
-                        fullWidth
-                        variant='contained'
-                        sx={{ mt: 3, mb: 2 }}
-                    >
+                    <TextField margin='normal' fullWidth label='Email Address' name='email' />
+                    <TextField margin='normal' fullWidth label='Password' name='password' />
+                    <TextField margin='normal' fullWidth label='Confirm Password' name='passwordValidation' />
+                    <Button type='submit' fullWidth variant='contained' sx={{ mt: 3, mb: 2 }}>
                         Sign In
                     </Button>
                     <Grid container>
-                        <Grid item>
+                        <Grid item xs>
                             <Link href='#' variant='body2'>
                                 Already have an account?
+                            </Link>
+                        </Grid>
+                        <Grid item>
+                            <Link href='#' variant='body2'>
+                                Sign up with Google.
                             </Link>
                         </Grid>
                     </Grid>
