be8aa19834e6d92448d9e48303d465b41810ca04
ketphan02 <tuankietvn@outlook.com>
2023-10-21 23:40:04 -0700

    This might work?

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -10,14 +10,15 @@ const SignInPage = () => {
     return (
         <div className={styles.signInPage} id='sign-in-page'>
             <h1>Sign In Page</h1>
-            <button className={styles.signInBtn} onClick={signInWithGoogle} data-cy='google-sign-in-btn'>
+            <button className={styles.signInBtn} onClick={(e) => signInWithGoogle(e)} data-cy='google-sign-in-btn'>
                 Sign in with Google
             </button>
         </div>
     )
 
-    function signInWithGoogle(): void {
+    function signInWithGoogle(e: React.MouseEvent<HTMLButtonElement>): void {
         try {
+            e.preventDefault()
             signIn('google').catch((error) => {
                 logger.error('An unexpected error occurred while log in with Google: ' + error)
             })
