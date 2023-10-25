ec771596b31aacfa26255981dd5baf1b1180cc11
Hedgemon4 <sethlinc4@gmail.com>
2023-10-25 16:23:11 -0700

    removed old css and updated cypress tests

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
-import { Box } from '@mui/material'
+import { Alert, Box } from '@mui/material'
+
+const boxCss = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+}
 
 export default function Home() {
     return (
-        <Box display='flex' justifyContent='center'>
+        <Box sx={boxCss}>
             <h1>Harp</h1>
+            <Alert severity='success'>This is now the home page</Alert>
         </Box>
     )
 }
