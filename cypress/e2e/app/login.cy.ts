c7d86d09437c797328cce3849d8fbdc639139c02
te-sa <teresa_saller@freenet.de>
2023-10-30 08:46:54 -0700

    Commented out unfinished tests

--- cypress/e2e/app/login.cy.ts
+++ cypress/e2e/app/login.cy.ts
@@ -15,38 +15,38 @@ describe('Login tests', () => {
         cy.url().should('include', '/login')
     })
 
-    it('Should give the user error feedback for an invalid email (pop up)', () => {
-        // Check that a valid email must be entered
-        cy.visit('/login')
-        cy.get('[data-cy="email"]').type('badEmail')
-        cy.get('[data-cy="submit"]').click()
-        cy.url().should('include', '/signup')
-    })
-
-    it('Should give the user error feedback for an invalid email (field error)', () => {
-        // Second email verification
-        cy.visit('/login')
-        cy.get('[data-cy="email"]').type('Incomplete@email')
-        cy.get('[data-cy="submit"]').click()
-        cy.url().should('include', '/signup')
-    })
-
-    it('Should give the user error feedback for a weak password', () => {
-        // Check that appropriate password form must be used
-        cy.visit('/login')
-        cy.get('[data-cy="password"]').type('notgood')
-        cy.get('[data-cy="submit"]').click()
-        cy.url().should('include', '/signup')
-    })
-
-    it('Should allow the login of a valid user', () => {
-        // Check that a valid user able to be created
-        cy.visit('/login')
-        cy.get('[data-cy="email"]').type('best@email.evr')
-        cy.get('[data-cy="password"]').type('SuchAG00dPassword!')
-        cy.get('[data-cy="submit"]').click()
-        cy.url().should('include', '/dashboard')
-    })
+    // it('Should give the user error feedback for an invalid email (pop up)', () => {
+    //     // Check that a valid email must be entered
+    //     cy.visit('/login')
+    //     cy.get('[data-cy="email"]').type('badEmail')
+    //     cy.get('[data-cy="submit"]').click()
+    //     cy.url().should('include', '/signup')
+    // })
+    //
+    // it('Should give the user error feedback for an invalid email (field error)', () => {
+    //     // Second email verification
+    //     cy.visit('/login')
+    //     cy.get('[data-cy="email"]').type('Incomplete@email')
+    //     cy.get('[data-cy="submit"]').click()
+    //     cy.url().should('include', '/signup')
+    // })
+    //
+    // it('Should give the user error feedback for a weak password', () => {
+    //     // Check that appropriate password form must be used
+    //     cy.visit('/login')
+    //     cy.get('[data-cy="password"]').type('notgood')
+    //     cy.get('[data-cy="submit"]').click()
+    //     cy.url().should('include', '/signup')
+    // })
+    //
+    // it('Should allow the login of a valid user', () => {
+    //     // Check that a valid user able to be created
+    //     cy.visit('/login')
+    //     cy.get('[data-cy="email"]').type('best@email.evr')
+    //     cy.get('[data-cy="password"]').type('SuchAG00dPassword!')
+    //     cy.get('[data-cy="submit"]').click()
+    //     cy.url().should('include', '/dashboard')
+    // })
 
     // TODO: Write tests for:
     // wrong password
