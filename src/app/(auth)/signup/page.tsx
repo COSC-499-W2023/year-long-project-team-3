3d091af849fbaac1255a4e16b6f840f4bba8d5ac
te-sa <teresa_saller@freenet.de>
2023-10-30 09:06:22 -0700

    Small fix

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,12 +1,12 @@
-import Form from '../../../components/SignUp/signUpForm'
 import UserAccountNav from '@/components/UserAccountNav/UserAccountNav'
+import SignUpForm from '../../../components/SignUp/signUpForm'
 
 export default async function SignUp() {
     // TODO: figure out what should happen if use navigates to signup while logged in
     return (
         <>
             <UserAccountNav />
-            <Form />
+            <SignUpForm />
         </>
     )
 }
