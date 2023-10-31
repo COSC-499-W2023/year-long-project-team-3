3fe7357f4bee431a3fc501e8efebadd5403f7e3d
justino599 <justin.schoenit@gmail.com>
2023-10-30 19:08:04 -0700

    Complete cypress tests

--- cypress/e2e/app/signup.cy.ts
+++ cypress/e2e/app/signup.cy.ts
@@ -8,75 +8,79 @@ describe('Sign up tests', () => {
     it('Should not allow the user to sign up with no information in fields', () => {
         // Check that submitting with nothing in the fields presents user with prompts and does not create a user
         cy.visit('/signup')
+
+        // Check that the errors do not exist
+        cy.get('p.Mui-error').should('have.length', 0)
+
         cy.get('[data-cy="submit"]').click()
 
         cy.url().should('include', '/signup')
 
-        cy.get('[data-cy="email"]').find('.Mui-error').should('be.visible').and('contain', 'Email is required')
-        cy.get('[data-cy="password"]').find('.Mui-error').should('be.visible').and('contain', 'Enter your password')
+        cy.get('p.Mui-error').should('have.length', 3)
+        cy.get('[data-cy="email"]').find('p.Mui-error').should('be.visible').and('contain', 'Email is required')
+        cy.get('[data-cy="password"]').find('p.Mui-error').should('be.visible').and('contain', 'Enter your password')
         cy.get('[data-cy="passwordVerification"]')
-            .find('.Mui-error')
+            .find('p.Mui-error')
             .should('be.visible')
             .and('contain', 'Please re-type your password')
     })
 
-    it('Should give the user error feedback for an invalid email (pop up)', () => {
+    it('Should give the user error feedback for an invalid email', () => {
         // user data
-        const email = 'badEmail'
-        const password = 'AreallyG00dPass!'
+        const testValues = [
+            { email: 'badEmail', expectedResponse: 'Enter a valid email' },
+            { email: 'incomplete@email', expectedResponse: 'Enter a valid email' },
+        ]
 
         // Check that a valid email must be entered
         cy.visit('/signup')
-        cy.get('[data-cy="email"]').type(email)
-        cy.get('[data-cy="password"]').type(password)
-        cy.get('[data-cy="submit"]').click()
 
-        cy.url().should('include', '/signup')
+        cy.wrap(testValues).each((input: { email: string; expectedResponse: string }) => {
+            cy.get('[data-cy="email"]').find('input').clear().type(input.email)
+            cy.get('[data-cy="submit"]').click()
 
-        cy.get('[data-cy="email"]').find('.Mui-error').should('be.visible').and('contain', 'Enter a valid email')
-    })
+            cy.url().should('include', '/signup')
 
-    it('Should give the user error feedback for an invalid email (field error)', () => {
-        // user data
-        const email = 'Incomplete@email'
-        const password = 'AreallyG00dPass!'
-
-        // Second email verification
-        cy.visit('/signup')
-        cy.get('[data-cy="email"]').type(email)
-        cy.get('[data-cy="password"]').type(password)
-        cy.get('[data-cy="submit"]').click()
-
-        cy.url().should('include', '/signup')
-
-        cy.get('[data-cy="email"]').find('.Mui-error').should('be.visible').and('contain', 'Enter a valid email')
+            cy.get('[data-cy="email"]').find('p.Mui-error').should('be.visible').and('contain', input.expectedResponse)
+        })
     })
 
     it('Should give the user error feedback for a weak password', () => {
         // user data
-        const email = 'best@email.evr'
-        const password = 'notgood'
+        const testValues = [
+            { pass: 'notgood', expectedResponse: 'Password should be a minimum of 8 characters long' },
+            {
+                pass: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+                expectedResponse: 'Password should be a maximum of 100 characters long',
+            },
+            { pass: 'Password', expectedResponse: 'Your password must have at least one number character.' },
+            { pass: 'passw0rd', expectedResponse: 'Your password must have at least one uppercase character.' },
+            { pass: 'PASSW0RD', expectedResponse: 'Your password must have at least one lowercase character.' },
+        ]
 
         // Check that appropriate password form must be used
         cy.visit('/signup')
-        cy.get('[data-cy="email"]').type(email)
-        cy.get('[data-cy="password"]').type(password)
-        cy.get('[data-cy="submit"]').click()
 
-        cy.url().should('include', '/signup')
+        cy.wrap(testValues).each((input: { pass: string; expectedResponse: string }) => {
+            cy.get('[data-cy="password"]').find('input').clear().type(input.pass)
+            cy.get('[data-cy="submit"]').click()
 
-        cy.get('[data-cy="password"]').find('.Mui-error').should('be.visible').and('contain', 'Password should')
+            cy.url().should('include', '/signup')
+
+            cy.get('[data-cy="password"]')
+                .find('p.Mui-error')
+                .should('be.visible')
+                .and('contain', input.expectedResponse)
+        })
     })
 
     it('Should give the user error feedback when both password fields do not match', () => {
         // User data
-        const userEmail = 'best@email.evr'
         const password = 'AreallyG00dPass!'
         const retypedPassword = 'N0ttHEsAMEP4ss@@@'
 
         // Check that Comfirmation password and password must match
         cy.visit('/signup')
-        cy.get('[data-cy="email"]').type(userEmail)
         expect(password).not.equal(retypedPassword)
         cy.get('[data-cy="password"]').type(password)
         cy.get('[data-cy="passwordVerification"]').type(retypedPassword)
@@ -85,12 +89,12 @@ describe('Sign up tests', () => {
         cy.url().should('include', '/signup')
 
         cy.get('[data-cy="passwordVerification"]')
-            .find('.Mui-error')
+            .find('p.Mui-error')
             .should('be.visible')
             .and('contain', 'Your passwords must match')
     })
 
-    it('Should allow the creation of a valid user', async () => {
+    it('Should allow the creation of a valid user', () => {
         // User data
         const userEmail = 'best@email.evr'
         const password = 'TryT0UseEmailAgain!'
@@ -102,12 +106,11 @@ describe('Sign up tests', () => {
         cy.get('[data-cy="passwordVerification"]').type(password)
         cy.get('[data-cy="submit"]').click()
 
-        // Test that user creation redirects to dashboard
-        // TODO: In next PR
-        // cy.url().should('contain', '/dashboard')
+        // We shouldn't be on the signup page anymore
+        cy.url().should('include', 'dashboard')
     })
 
-    it.only('Should not allow the creation of an account that already is using email', () => {
+    it.skip('Should not allow the creation of an account that already is using email', () => {
         // User data
         const userEmail = 'best@email.evr'
         const password = 'TryT0UseEmailAgain!'
@@ -118,10 +121,7 @@ describe('Sign up tests', () => {
         cy.get('[data-cy="password"]').type(password)
         cy.get('[data-cy="passwordVerification"]').type(password)
         cy.get('[data-cy="submit"]').click()
-
-        cy.url().should('not.contain', 'signup')
-
-        // TODO: Log out
+        cy.url().should('include', '/')
 
         // Check that already in use email cannot be used to sign up again
         cy.visit('/signup')
@@ -132,6 +132,6 @@ describe('Sign up tests', () => {
 
         cy.url().should('include', '/signup')
 
-        cy.get('[]')
+        cy.get('.Toastify__toast-container').should('be.visible').and('contain', 'This email address is already in use')
     })
 })
