7a67025df5e7959da7db982c0115f51625f2c5ad
te-sa <teresa_saller@freenet.de>
2023-10-30 19:45:35 -0700

    Changed tests

--- cypress/e2e/app/login.cy.ts
+++ cypress/e2e/app/login.cy.ts
@@ -1,59 +1,41 @@
 describe('Login tests', () => {
     before(() => {
         cy.task('clearDB')
-        // cy.intercept({
-        //     method: 'GET',
-        //     url: '',
-        // }).as('login')
-    })
-
-    it('Should load up the login page', () => {
-        // Check page loads
-        cy.visit('/login')
     })
 
     it('Should not allow the user to log in with no information in fields', () => {
-        // Check that submitting with nothing in the fields presents user with prompts and does not log the user in
+        // Check that submitting with nothing in the fields presents user with prompts and does not create a user
         cy.visit('/login')
+
+        // Check that the errors do not exist
+        cy.get('p.Mui-error').should('have.length', 0)
+
         cy.get('[data-cy="submit"]').click()
+
         cy.url().should('include', '/login')
+
+        cy.get('p.Mui-error').should('have.length', 2)
+        cy.get('[data-cy="email"]').find('p.Mui-error').should('be.visible').and('contain', 'Email is required')
+        cy.get('[data-cy="password"]').find('p.Mui-error').should('be.visible').and('contain', 'Enter your password')
     })
 
-    it('Should give the user error feedback for an invalid email (pop up)', () => {
+    it('Should give the user error feedback for an invalid email', () => {
+        // user data
+        const testValues = [
+            { email: 'badEmail', expectedResponse: 'Enter a valid email' },
+            { email: 'incomplete@email', expectedResponse: 'Enter a valid email' },
+        ]
+
         // Check that a valid email must be entered
         cy.visit('/login')
-        cy.get('[data-cy="email"]').type('badEmail')
-        cy.get('[data-cy="submit"]').click()
-        cy.url().should('include', '/login')
-    })
 
-    it('Should give the user error feedback for an invalid email (field error)', () => {
-        // Second email verification
-        cy.visit('/login')
-        cy.get('[data-cy="email"]').type('Incomplete@email')
-        cy.get('[data-cy="submit"]').click()
-        cy.url().should('include', '/login')
-    })
+        cy.wrap(testValues).each((input: { email: string; expectedResponse: string }) => {
+            cy.get('[data-cy="email"]').find('input').clear().type(input.email)
+            cy.get('[data-cy="submit"]').click()
 
-    it('Should give the user error feedback for a weak password', () => {
-        // Check that appropriate password form must be used
-        cy.visit('/login')
-        cy.get('[data-cy="password"]').type('notgood')
-        cy.get('[data-cy="submit"]').click()
-        cy.url().should('include', '/login')
-    })
+            cy.url().should('include', '/login')
 
-    // need to mock user
-    it('Should allow the login of a valid user', () => {
-        // Check that a valid user able to be created
-        cy.visit('/login')
-        cy.get('[data-cy="email"]').type('best@email.evr')
-        cy.get('[data-cy="password"]').type('SuchAG00dPassword!')
-        cy.get('[data-cy="submit"]').click()
-        cy.url().should('include', '/dashboard')
+            cy.get('[data-cy="email"]').find('p.Mui-error').should('be.visible').and('contain', input.expectedResponse)
+        })
     })
-
-    // TODO: Write tests for:
-    // wrong password
-    // user does not exist
 })
