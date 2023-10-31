e4ed61e5b8115f94c5b3fefd6d54fe49b706afec
ketphan02 <tuankietvn@outlook.com>
2023-10-30 22:06:51 -0700

    Fix missing secret

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -20,7 +20,7 @@ export const authOptions: NextAuthOptions = {
             child.error(msg)
         },
     },
-    secret: process.env.nextAuthSecret,
+    secret: process.env.nextAuthSecret ?? 'secret',
     pages: {
         signIn: '/login',
     },
