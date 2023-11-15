dd325e3861096d3ac1f2a131330e3b98049e82f0
justino599 <justin.schoenit@gmail.com>
2023-11-14 21:52:20 -0800

    Video keyboard controls

--- src/components/ScalingReactPlayer/index.tsx
+++ src/components/ScalingReactPlayer/index.tsx
@@ -1,14 +1,24 @@
 import dynamic from 'next/dynamic'
 
-const ReactPlayer = dynamic(() => import('react-player/lazy'), { ssr: false })
+const VideoPlayer = dynamic(() => import('../VideoPlayer'), { ssr: false })
 import { Box } from '@mui/material'
-import React, { useEffect } from 'react'
+import React, { MutableRefObject, useEffect, useRef, useState } from 'react'
+import ReactPlayer from 'react-player/lazy'
+import screenfull from 'screenfull'
 
 export type ScalingReactPlayerProps = {
     url: string
 }
 
 const ScalingReactPlayer = (props: ScalingReactPlayerProps) => {
+    const [state, setState] = useState({
+        isPlaying: false,
+        isMuted: false,
+        volume: 1.0,
+        playbackRate: 1.0,
+    })
+    const playerRef = useRef() as MutableRefObject<ReactPlayer>
+
     const updateSize = () => {
         const rw = document.querySelector('.react-wrapper')
 
@@ -28,17 +38,73 @@ const ScalingReactPlayer = (props: ScalingReactPlayerProps) => {
     }
 
     useEffect(() => {
+        const keyboardControl = (evt: KeyboardEvent) => {
+            /*
+            Toggle play/pause the video	k or Spacebar
+            Go back 5 seconds	Left arrow
+            Go back 10 seconds	j
+            Go forward 5 seconds	Right arrow
+            Go forward 10 seconds	l
+            Restart video	Home
+            Bring video to the end	End
+            Go to Full Screen mode	f
+            Exit Full Screen mode	Escape
+            Increase volume 5%	Up arrow
+            Decrease volume 5%	Down arrow
+            Increase speed	Shift + >
+            Decrease speed	Shift + <
+             */
+            const code = evt.code
+            if (code === 'KeyK' || code === 'Space') {
+                setState({ ...state, isPlaying: !state.isPlaying })
+            } else if (code === 'KeyM') {
+                setState({ ...state, isMuted: !state.isMuted })
+            } else if (code === 'ArrowUp') {
+                setState({ ...state, volume: Math.min(1, state.volume + 0.05) })
+            } else if (code === 'ArrowDown') {
+                setState({ ...state, volume: Math.max(0, state.volume - 0.05) })
+            } else if (code === 'ArrowRight') {
+                playerRef.current &&
+                    playerRef.current.seekTo(
+                        Math.min(playerRef.current.getDuration(), playerRef.current.getCurrentTime() + 5)
+                    )
+            } else if (code === 'ArrowLeft') {
+                playerRef.current && playerRef.current.seekTo(Math.max(0, playerRef.current.getCurrentTime() - 5))
+            } else if (code === 'KeyF') {
+                screenfull.exit().then(() => {})
+                const player = document.querySelector('.react-player > video')
+                player && screenfull.request(player)
+            } else if (code === 'KeyL') {
+                playerRef.current &&
+                    playerRef.current.seekTo(
+                        Math.min(playerRef.current.getDuration(), playerRef.current.getCurrentTime() + 10)
+                    )
+            } else if (code === 'KeyJ') {
+                playerRef.current && playerRef.current.seekTo(Math.max(0, playerRef.current.getCurrentTime() - 10))
+            } else if (code === 'Home') {
+                playerRef.current && playerRef.current.seekTo(0)
+            } else if (code === 'End') {
+                playerRef.current && playerRef.current.seekTo(playerRef.current.getDuration())
+            } else if (code === 'Period' && evt.shiftKey) {
+                setState({ ...state, playbackRate: Math.min(3, state.playbackRate + 0.25) })
+            } else if (code === 'Comma' && evt.shiftKey) {
+                setState({ ...state, playbackRate: Math.max(0.25, state.playbackRate - 0.25) })
+            }
+        }
+
         // Initial size update
         updateSize()
 
-        // Add event listener for viewport resize
+        // Add event listeners
+        window.addEventListener('keydown', keyboardControl)
         window.addEventListener('resize', updateSize)
 
-        // Cleanup the event listener on component unmount
+        // Cleanup the listeners on component unmount
         return () => {
+            window.removeEventListener('keydown', keyboardControl)
             window.removeEventListener('resize', updateSize)
         }
-    }, [])
+    }, [state, playerRef])
 
     return (
         <Box
@@ -48,7 +114,18 @@ const ScalingReactPlayer = (props: ScalingReactPlayerProps) => {
                 backgroundColor: 'black',
             }}
         >
-            <ReactPlayer className='react-player' url={props.url} controls width='100%' height='100%' />
+            <VideoPlayer
+                playerref={playerRef}
+                className='react-player'
+                url={props.url}
+                playing={state.isPlaying}
+                muted={state.isMuted}
+                volume={state.volume}
+                playbackRate={state.playbackRate}
+                controls
+                width='100%'
+                height='100%'
+            />
         </Box>
     )
 }
