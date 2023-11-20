b0ee302582a218bab40fbe36c4e25ca1f62b901c
justino599 <justin.schoenit@gmail.com>
2023-11-19 22:39:55 -0800

    Implement rest of editing menus

--- src/components/EditorTools/index.tsx
+++ src/components/EditorTools/index.tsx
@@ -8,9 +8,11 @@ import {
     Typography,
     Tooltip,
     Button,
+    Slider,
 } from '@mui/material'
 import React, { useState } from 'react'
 import { ContentCut, Speed, TuneRounded, VolumeOff, VolumeUp } from '@mui/icons-material'
+import TimestampInputField from '@/components/TimestampInputField'
 
 export type EditorToolsProps = {
     handleHaveChangesBeenMade: (haveChangesBeenMade: boolean) => void
@@ -21,12 +23,18 @@ const EditorTools = (props: EditorToolsProps) => {
     const [isBlurred, setIsBlurred] = useState(false)
     const [isBWVideo, setIsBWVideo] = useState(false)
     const [isOtherOption, setIsOtherOption] = useState(false)
+    const [startTime, setStartTime] = useState('')
+    const [endTime, setEndTime] = useState('')
+    const [playbackRate, setPlaybackRate] = useState(1.0)
 
     const [startingState, setStartingState] = useState({
         isMuted: false,
         isBlurred: false,
         isBWVideo: false,
         isOtherOption: false,
+        startTime: '',
+        endTime: '',
+        playbackRate: 1.0,
     })
 
     const [isFilterOpen, setIsFilterOpen] = useState(false)
@@ -67,6 +75,9 @@ const EditorTools = (props: EditorToolsProps) => {
             isBlurred: isBlurred,
             isBWVideo: isBWVideo,
             isOtherOption: isOtherOption,
+            startTime: startTime,
+            endTime: endTime,
+            playbackRate: playbackRate,
         })
 
         // TODO: Actually apply changes to video
@@ -77,7 +88,10 @@ const EditorTools = (props: EditorToolsProps) => {
             isMuted === startingState.isMuted &&
             isBlurred === startingState.isBlurred &&
             isBWVideo === startingState.isBWVideo &&
-            isOtherOption === startingState.isOtherOption
+            isOtherOption === startingState.isOtherOption &&
+            startTime === startingState.startTime &&
+            endTime === startingState.endTime &&
+            playbackRate === startingState.playbackRate
         )
     }
 
@@ -88,12 +102,23 @@ const EditorTools = (props: EditorToolsProps) => {
         top: '50%',
         left: '50%',
         transform: 'translate(-50%, -50%)',
-        width: '40vw',
+        width: '30vw',
         minWidth: '20rem',
         backgroundColor: 'background.default',
         borderRadius: '1rem',
         boxShadow: 24,
         padding: '1rem 2rem',
+    }
+
+    const parseSeconds = (timestamp: string) => {
+        const parts = timestamp.split(':')
+        let seconds = 0
+        let multiplier = 1
+        for (let i = parts.length - 1; i >= 0; i--) {
+            seconds += parseFloat(parts[i]) * multiplier
+            multiplier *= 60
+        }
+        return seconds
     }
 
     return (
@@ -106,48 +131,6 @@ const EditorTools = (props: EditorToolsProps) => {
                     alignItems: 'flex-start',
                 }}
             >
-                {/*<Box*/}
-                {/*    sx={{*/}
-                {/*        display: 'flex',*/}
-                {/*        flexDirection: 'row',*/}
-                {/*        gap: '0.3rem',*/}
-                {/*    }}*/}
-                {/*>*/}
-                {/*    /!*Put invisible button to match spacing of buttons below the title*!/*/}
-                {/*    <IconButton sx={{ visibility: 'hidden' }}>*/}
-                {/*        <SquareIcon />*/}
-                {/*    </IconButton>*/}
-                {/*    <Typography*/}
-                {/*        color='black'*/}
-                {/*        variant='h6'*/}
-                {/*        component='div'*/}
-                {/*        textAlign='center'*/}
-                {/*        sx={{*/}
-                {/*            margin: 0,*/}
-                {/*            flexGrow: 1,*/}
-                {/*            display: 'flex',*/}
-                {/*            flexDirection: 'column',*/}
-                {/*            justifyContents: 'center',*/}
-                {/*        }}*/}
-                {/*    >*/}
-                {/*        Video Options*/}
-                {/*    </Typography>*/}
-                {/*</Box>*/}
-                {/*<Box*/}
-                {/*    sx={{*/}
-                {/*        display: 'flex',*/}
-                {/*        flexDirection: 'row',*/}
-                {/*        flexGrow: 1,*/}
-                {/*        gap: '0.3rem',*/}
-                {/*    }}*/}
-                {/*>*/}
-                {/*<Box*/}
-                {/*    className='editor-tool-buttons'*/}
-                {/*    sx={{*/}
-                {/*        display: 'flex',*/}
-                {/*        flexDirection: 'column',*/}
-                {/*    }}*/}
-                {/*>*/}
                 <Tooltip title='Mute' placement='right'>
                     <IconButton onClick={handleMuteClick}>{isMuted ? <VolumeUp /> : <VolumeOff />}</IconButton>
                 </Tooltip>
@@ -172,49 +155,37 @@ const EditorTools = (props: EditorToolsProps) => {
                         <Speed />
                     </IconButton>
                 </Tooltip>
-                {/*</Box>*/}
-                {/*<Box*/}
-                {/*    sx={{*/}
-                {/*        flexGrow: 1,*/}
-                {/*        display: 'flex',*/}
-                {/*        flexDirection: 'column',*/}
-                {/*        alignItems: 'center',*/}
-                {/*        gap: '0.5rem',*/}
-                {/*    }}*/}
-                {/*>*/}
-                {/*    <Box*/}
-                {/*        className='editor-options'*/}
-                {/*        sx={{*/}
-                {/*            flexGrow: 1,*/}
-                {/*            backgroundColor: 'primary.lighter',*/}
-                {/*            borderRadius: '1rem',*/}
-                {/*            padding: '0.5rem 0.8rem',*/}
-                {/*            width: '100%',*/}
-                {/*        }}*/}
-                {/*    ></Box>*/}
-                {/*</Box>*/}
-                {/*</Box>*/}
-                {/*<Button variant='outlined' disabled={!changesMade()} onClick={handleApplyChangesClick}>*/}
-                {/*    Apply Changes*/}
-                {/*</Button>*/}
             </Box>
             <Modal open={isFilterOpen} onClose={handleFilterClose}>
                 <Box sx={modalStyle}>
-                    <FormGroup>
+                    <Typography
+                        component='div'
+                        variant='h5'
+                        sx={{
+                            textAlign: 'center',
+                        }}
+                    >
+                        Video Filters
+                    </Typography>
+                    <FormGroup
+                        sx={{
+                            margin: '1.5rem 0 0.5rem 0',
+                        }}
+                    >
                         <FormControlLabel
                             control={<Checkbox size='small' name='blur' checked={isBlurred} />}
                             label='Blur Face'
-                            onChange={(e, checked) => setIsBlurred(checked)}
+                            onChange={(_e, checked) => setIsBlurred(checked)}
                         />
                         <FormControlLabel
                             control={<Checkbox size='small' name='bwVideo' checked={isBWVideo} />}
                             label='Black & White Video'
-                            onChange={(e, checked) => setIsBWVideo(checked)}
+                            onChange={(_e, checked) => setIsBWVideo(checked)}
                         />
                         <FormControlLabel
                             control={<Checkbox size='small' name='other' checked={isOtherOption} />}
                             label='Another option'
-                            onChange={(e, checked) => setIsOtherOption(checked)}
+                            onChange={(_e, checked) => setIsOtherOption(checked)}
                         />
                     </FormGroup>
                     <Box
@@ -231,22 +202,90 @@ const EditorTools = (props: EditorToolsProps) => {
             </Modal>
             <Modal open={isCutOpen} onClose={handleCutClose}>
                 <Box sx={modalStyle}>
-                    <Typography>Video trimming menu</Typography>
+                    <Typography
+                        component='div'
+                        variant='h5'
+                        sx={{
+                            textAlign: 'center',
+                        }}
+                    >
+                        Video Trimming
+                    </Typography>
                     <Box
                         sx={{
                             display: 'flex',
                             flexDirection: 'row',
-                            justifyContent: 'flex-end',
+                            justifyContent: 'flex-start',
+                            gap: '1rem',
+                            width: '100%',
+                            margin: '1.5rem 0 0.5rem 0',
+                        }}
+                    >
+                        <TimestampInputField
+                            label='Start Time'
+                            value={startTime}
+                            onChange={(value) => setStartTime(value)}
+                        />
+                        <TimestampInputField label='End Time' value={endTime} onChange={(value) => setEndTime(value)} />
+                    </Box>
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'row',
+                            justifyContent: 'space-between',
+                            alignItems: 'center',
                             width: '100%',
                         }}
                     >
+                        <Typography
+                            color='red'
+                            sx={{
+                                visibility:
+                                    startTime === '' ||
+                                    endTime === '' ||
+                                    parseSeconds(startTime) < parseSeconds(endTime)
+                                        ? 'hidden'
+                                        : 'inline',
+                            }}
+                        >
+                            Start time must be less than end time
+                        </Typography>
                         <Button onClick={handleCutClose}>Done</Button>
                     </Box>
                 </Box>
             </Modal>
             <Modal open={isPlaybackRateOpen} onClose={handlePlaybackRateClose}>
                 <Box sx={modalStyle}>
-                    <Typography>Video playback rate</Typography>
+                    <Typography
+                        component='div'
+                        variant='h5'
+                        sx={{
+                            textAlign: 'center',
+                        }}
+                    >
+                        Video playback rate
+                    </Typography>
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'row',
+                            justifyContent: 'flex-start',
+                            gap: '1rem',
+                            width: '100%',
+                            margin: '1.5rem 0 0.5rem 0',
+                        }}
+                    ></Box>
+                    <Slider
+                        value={playbackRate}
+                        step={0.25}
+                        marks={Array.from({ length: 12 }, (_, index) => ({
+                            value: 0.25 * (index + 1),
+                            label: index % 2 == 1 ? 0.25 * (index + 1) : '',
+                        }))}
+                        min={0.25}
+                        max={3}
+                        onChange={(_event, value, _i) => setPlaybackRate(typeof value === 'number' ? value : value[0])}
+                    />
                     <Box
                         sx={{
                             display: 'flex',
