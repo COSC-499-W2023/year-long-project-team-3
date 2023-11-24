c641d10a6e1f011904f6a240ef08998dc42a0827
ketphan02 <tuankietvn@outlook.com>
2023-11-23 19:03:04 -0800

    Remove aws v2 and use v3

--- src/utils/sendVideo.ts
+++ src/utils/sendVideo.ts
@@ -1,8 +1,13 @@
 import { User, Video } from '@prisma/client'
-import AWS from 'aws-sdk'
-import S3, { PutObjectRequest } from 'aws-sdk/clients/s3'
+import {
+    S3Client,
+    type CompleteMultipartUploadCommandOutput,
+    type AbortMultipartUploadCommandOutput,
+} from '@aws-sdk/client-s3'
+import { Upload } from '@aws-sdk/lib-storage'
 import logger from './logger'
 import prisma from '@/lib/prisma'
+import { Readable } from 'stream'
 
 async function makeS3Key(video: Video): Promise<string> {
     if (!video.ownerId) {
@@ -22,17 +27,15 @@ function _removeFileExtension(filename: string): string {
     return filename.replace(/\.[^/.]+$/, '')
 }
 
-export default async function sendVideo(rawVideo: File, owner: User): Promise<Video> {
-    AWS.config.update({
-        region: process.env.awsUploadRegion,
-        credentials: {
-            accessKeyId: process.env.awsAccessKeyId as string,
-            secretAccessKey: process.env.awsSecretAccessKey as string,
-            sessionToken: process.env.awsSessionToken as string,
-        },
-    })
-    const s3 = new S3()
 
+// Fom https://stackoverflow.com/a/76640098/13337984
+function isCompleteUpload(
+    output: CompleteMultipartUploadCommandOutput | AbortMultipartUploadCommandOutput
+): output is CompleteMultipartUploadCommandOutput {
+    return (output as CompleteMultipartUploadCommandOutput).ETag !== undefined
+}
+
+export default async function sendVideo(rawVideo: File, owner: User): Promise<Video> {
     const newVideo: Video = await prisma.video.create({
         data: {
             owner: {
@@ -49,50 +52,58 @@ export default async function sendVideo(rawVideo: File, owner: User): Promise<Vi
 
     const s3Key = await makeS3Key(newVideo)
     const rawVideoBuffer = await rawVideo.arrayBuffer()
-    const uploadParams: PutObjectRequest = {
-        Bucket: process.env.awsUploadBucket as string,
-        Key: s3Key,
-        Body: Buffer.from(rawVideoBuffer),
+
+    const client = new S3Client({
+        region: process.env.awsUploadRegion,
+        credentials: {
+            accessKeyId: process.env.awsAccessKeyId as string,
+            secretAccessKey: process.env.awsSecretAccessKey as string,
+            sessionToken: process.env.awsSessionToken as string,
+        },
+    })
+
+    const uploadS3 = new Upload({
+        client: client,
+        params: {
+            Bucket: process.env.awsUploadBucket as string,
+            Key: s3Key,
+            Body: Readable.from(Buffer.from(rawVideoBuffer)),
+        },
+    })
+
+    const s3Data = await uploadS3.done()
+    if (!isCompleteUpload(s3Data) || s3Data.Location === undefined) {
+        logger.error(s3Data)
+        // TODO: Send email/notify to user there is an issue with uploading video
+        throw new Error(`Unexpected error while uploading video ${ rawVideo.name } to S3`)
     }
 
-    s3.upload(uploadParams, async (err: Error, data: any) => {
-        if (err) {
-            logger.error(err)
-            // TODO: Send email/notify to user there is an issue with uploading video
-            throw new Error(`Unexpected error while uploading video ${ rawVideo.name } to S3`)
-        }
-
-        await prisma.videoWhitelist.create({
-            data: {
-                video: {
-                    connect: {
-                        id: newVideo.id,
-                    },
+    await prisma.videoWhitelist.create({
+        data: {
+            video: {
+                connect: {
+                    id: newVideo.id,
                 },
-                whitelistedUsers: {
-                    create: {
-                        whitelistedUser: {
-                            connect: {
-                                id: owner.id,
-                            },
+            },
+            whitelistedUsers: {
+                create: {
+                    whitelistedUser: {
+                        connect: {
+                            id: owner.id,
                         },
                     },
                 },
             },
-        })
-
-        await prisma.video.update({
-            where: {
-                id: newVideo.id,
-            },
-            data: {
-                s3Key: s3Key,
-                rawVideoUrl: data.Location,
-            },
-        })
-
-        logger.info(`File ${ newVideo.id } uploaded to S3 successfully.`)
+        },
     })
 
-    return newVideo
+    return prisma.video.update({
+        where: {
+            id: newVideo.id,
+        },
+        data: {
+            s3Key: s3Key,
+            rawVideoUrl: s3Data.Location,
+        },
+    })
 }
