5df10549756b8e20c23ac897e480dcaf5b25e90f
te-sa <teresa_saller@freenet.de>
2023-11-25 14:13:58 -0800

    Started implementing the settings form

---
+++ src/app/submission-box/settings/page.tsx
@@ -0,0 +1,124 @@
+'use client'
+
+import { useSession } from 'next-auth/react'
+import Header from '@/components/Header'
+import ProgressDots from '@/components/ProgressDots'
+import { Box } from '@mui/material'
+import Typography from '@mui/material/Typography'
+import React from 'react'
+import TextField from '@mui/material/TextField'
+import { useFormik } from 'formik'
+import logger from '@/utils/logger'
+
+interface FormValues {
+    title: string
+    description: string
+    closingDate: Date
+}
+
+export default function SubmissionBoxSettingsPage() {
+    const session = useSession()
+
+    const formik = useFormik<FormValues>({
+        initialValues: {
+            title: '',
+            description: '',
+            closingDate: new Date(),
+        },
+        // validationSchema: validationSchema,
+        // TODO
+        onSubmit: () => handleSubmit(),
+    })
+
+    return (
+        <>
+            <Header {...session} />
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    gap: '1rem',
+                    width: '100%',
+                    height: '100%',
+                    padding: '2rem',
+                }}
+            >
+                <Box
+                    sx={{
+                        minWidth: '16rem',
+                        width: '50%',
+                    }}
+                >
+                    <ProgressDots activeStep={0} numSteps={3} labels={['Settings', 'Add Members', 'Create']} />
+                </Box>
+                <Box display='flex' width='100%' flexDirection='column' alignItems='center' sx={{ pt: 3 }}>
+                    <Typography data-cy='title' variant='h4' sx={{ fontWeight: 'medium' }}>
+                        Settings
+                    </Typography>
+                    <form onSubmit={formik.handleSubmit} noValidate>
+                        <Box
+                            gap={1}
+                            sx={{
+                                p: 5,
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignItems: 'center',
+                                minWidth: 'md',
+                                '& .MuiTextField-root': { my: 1.5, mx: 7, width: '100%' },
+                            }}
+                        >
+                            <TextField
+                                margin='normal'
+                                variant='outlined'
+                                type='title'
+                                label='Enter a title for your submission box'
+                                name='title'
+                                value={formik.values.title}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                error={formik.touched.title && Boolean(formik.errors.title)}
+                                // this ensures the layout does not get shifted by the helper text
+                                FormHelperTextProps={{ style: { position: 'absolute', bottom: -20 } }}
+                                helperText={formik.touched.title && formik.errors.title}
+                                data-cy='title'
+                            />
+                            <TextField
+                                margin='normal'
+                                variant='outlined'
+                                type='description'
+                                label='Enter a description for your submission box (optional)'
+                                name='description'
+                                multiline
+                                rows={4}
+                                value={formik.values.description}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                error={formik.touched.description && Boolean(formik.errors.description)}
+                                // this ensures the layout does not get shifted by the helper text
+                                FormHelperTextProps={{ style: { position: 'absolute', bottom: -20 } }}
+                                helperText={formik.touched.description && formik.errors.description}
+                                data-cy='description'
+                                sx={{
+                                    // this ensures the label wraps
+                                    '& .MuiInputLabel-root': {
+                                        whiteSpace: 'pre-wrap',
+                                    },
+                                }}
+                            />
+                        </Box>
+                    </form>
+                </Box>
+            </Box>
+        </>
+    )
+
+    async function handleSubmit() {
+        try {
+            // TODO
+        } catch (err) {
+            const errMessage = JSON.stringify(err, Object.getOwnPropertyNames(err))
+            logger.error(errMessage)
+        }
+    }
+}
