9a061d4f8eace9324a4d8e1047b5aac7109ae31f
justino599 <justin.schoenit@gmail.com>
2023-11-26 23:23:47 -0800

    Add submission box to schema

---
+++ prisma/migrations/20231127071825_add_submission_box/migration.sql
@@ -0,0 +1,74 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `submittedAt` on the `Video` table. All the data in the column will be lost.
+
+*/
+-- CreateEnum
+CREATE TYPE "PrivilegeLevel" AS ENUM ('owner', 'admin');
+
+-- AlterTable
+ALTER TABLE "Video" DROP COLUMN "submittedAt";
+
+-- CreateTable
+CREATE TABLE "SubmissionBox" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "closesAt" TIMESTAMP(3),
+    "videoStoreToDate" TIMESTAMP(3),
+    "maxVideoLength" INTEGER,
+    "isPublic" BOOLEAN NOT NULL DEFAULT false,
+    "invitedEmails" TEXT[],
+
+    CONSTRAINT "SubmissionBox_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "RequestedSubmission" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "submissionBoxId" TEXT NOT NULL,
+    "requestedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "submittedAt" TIMESTAMP(3),
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "RequestedSubmission_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "SubmittedVideo" (
+    "videoId" TEXT NOT NULL,
+    "requestedSubmissionId" TEXT NOT NULL,
+    "submittedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "SubmittedVideo_pkey" PRIMARY KEY ("videoId","requestedSubmissionId")
+);
+
+-- CreateTable
+CREATE TABLE "SubmissionBoxManager" (
+    "userId" TEXT NOT NULL,
+    "submissionBoxId" TEXT NOT NULL,
+    "privilegeLevel" "PrivilegeLevel" NOT NULL,
+
+    CONSTRAINT "SubmissionBoxManager_pkey" PRIMARY KEY ("userId","submissionBoxId")
+);
+
+-- AddForeignKey
+ALTER TABLE "RequestedSubmission" ADD CONSTRAINT "RequestedSubmission_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "RequestedSubmission" ADD CONSTRAINT "RequestedSubmission_submissionBoxId_fkey" FOREIGN KEY ("submissionBoxId") REFERENCES "SubmissionBox"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "SubmittedVideo" ADD CONSTRAINT "SubmittedVideo_videoId_fkey" FOREIGN KEY ("videoId") REFERENCES "Video"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "SubmittedVideo" ADD CONSTRAINT "SubmittedVideo_requestedSubmissionId_fkey" FOREIGN KEY ("requestedSubmissionId") REFERENCES "RequestedSubmission"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "SubmissionBoxManager" ADD CONSTRAINT "SubmissionBoxManager_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "SubmissionBoxManager" ADD CONSTRAINT "SubmissionBoxManager_submissionBoxId_fkey" FOREIGN KEY ("submissionBoxId") REFERENCES "SubmissionBox"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
