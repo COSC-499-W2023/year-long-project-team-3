66aa4c933dac710e40c25e83734040edc206932e
te-sa <teresa_saller@freenet.de>
2023-11-26 22:14:39 -0800

    Made layout match

--- src/components/OwnerCard/index.tsx
+++ src/components/OwnerCard/index.tsx
@@ -9,37 +9,41 @@ export type MemberCardProps = {
 
 export default function OwnerCard(props: MemberCardProps) {
     return (
-        <>
-            <Card // This is the owner card, it cannot be removed
-                sx={{ width: '25rem', borderRadius: 12 }}
+        <Card // This is the owner card, it cannot be removed
+            sx={{ width: '25rem', borderRadius: 12, mb: '1rem' }}
+        >
+            <CardContent
+                sx={{
+                    px: 3,
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'space-between',
+                }}
             >
-                <CardContent sx={{ px: 3, display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
-                    <Typography
-                        sx={{
-                            pt: 1,
-                            maxWidth: '70%',
-                            overflow: 'hidden',
-                            textOverflow: 'ellipsis',
-                            whiteSpace: 'nowrap',
-                        }}
-                    >
-                        {props.ownerEmail}
-                    </Typography>
-                    <Box
-                        sx={{
-                            display: 'flex',
-                            flexDirection: 'row',
-                            alignItems: 'center',
-                        }}
-                    >
-                        <Typography sx={{ pt: 1, pr: 2 }}>Owner</Typography>
-                        <IconButton size='small' disabled sx={{ mt: 1, backgroundColor: '#F5F5F5' }}>
-                            <Remove />
-                        </IconButton>
-                    </Box>
-                </CardContent>
-            </Card>
-            <Box sx={{ mb: '1rem' }}></Box>
-        </>
+                <Typography
+                    sx={{
+                        pt: 1,
+                        maxWidth: '70%',
+                        overflow: 'hidden',
+                        textOverflow: 'ellipsis',
+                        whiteSpace: 'nowrap',
+                    }}
+                >
+                    {props.ownerEmail}
+                </Typography>
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Typography sx={{ pt: 1, pr: 2 }}>Owner</Typography>
+                    <IconButton disabled size='small' sx={{ mt: 1, backgroundColor: '#F5F5F5' }}>
+                        <Remove />
+                    </IconButton>
+                </Box>
+            </CardContent>
+        </Card>
     )
 }
