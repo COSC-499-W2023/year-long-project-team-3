e3cd174bce2d3a340c640e3bfb8f04cb9369d84b
te-sa <teresa_saller@freenet.de>
2023-11-26 16:38:03 -0800

    Added basic layout to pages

--- src/app/submission-box/add-members/page.tsx
+++ src/app/submission-box/add-members/page.tsx
@@ -3,13 +3,34 @@
 import { useSession } from 'next-auth/react'
 import Header from '@/components/Header'
 import React from 'react'
+import { Box } from '@mui/material'
+import ProgressDots from '@/components/ProgressDots'
 
-// TODO: Implement this page (right now it's only a bare bones version for testing)
 export default function SubmissionBoxAddMembersPage() {
     const session = useSession()
     return (
         <>
             <Header {...session} />
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    gap: '1rem',
+                    width: '100%',
+                    height: '100%',
+                    padding: '2rem',
+                }}
+            >
+                <Box
+                    sx={{
+                        minWidth: '16rem',
+                        width: '50%',
+                    }}
+                >
+                    <ProgressDots activeStep={1} numSteps={3} labels={['Settings', 'Add Members', 'Create']} />
+                </Box>
+            </Box>
         </>
     )
 }
