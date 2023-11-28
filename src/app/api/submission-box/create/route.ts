ac9dff000edc8863b97f13bdf43d12648b37c34f
justino599 <justin.schoenit@gmail.com>
2023-11-28 15:34:57 -0800

    more stuff

--- src/app/api/submission-box/create/route.ts
+++ src/app/api/submission-box/create/route.ts
@@ -13,8 +13,12 @@ export async function POST(req: NextRequest): Promise<NextResponse> {
         const formData = await req.formData()
         let data = Object.fromEntries(formData.entries())
 
-        // Check for required fields
+        // Validate request fields
         if (!data || !data.title || typeof data.title !== 'string') {
+            return NextResponse.json({ error: 'Invalid Request' }, { status: 400 })
+        }
+
+        if (!data.invites) {
             return NextResponse.json({ error: 'Invalid Request' }, { status: 400 })
         }
 
@@ -22,16 +26,34 @@ export async function POST(req: NextRequest): Promise<NextResponse> {
             return NextResponse.json({ error: 'Invalid Request' }, { status: 400 })
         }
 
-        // const newSubmissionBox = await prisma.submissionBox.create({
-        //     data: {
-        //         title: data.title,
-        //         ...data,
-        //     },
-        // })
-
-        {
-            return NextResponse.json({ data: data }, { status: 201 })
+        if (data.closingDate && typeof data.closingDate !== 'string') {
+            return NextResponse.json({ error: 'Invalid Request' }, { status: 400 })
         }
+
+        // Create submusion box
+        const newSubmissionBox = await prisma.submissionBox.create({
+            data: {
+                title: data.title,
+                ...data,
+            },
+        })
+        const submissionBoxId = newSubmissionBox.id
+
+        // Create submission requests
+        const requests = await prisma.requestedSubmission.createMany({
+            data: (data.invites as string[]).map((email) => {
+                return { email: email, submissionBoxId: submissionBoxId }
+            }),
+        })
+
+        // Add logged-in user as manager
+        const ownerUserId = prisma.user.findUniqueOrThrow({
+            where: {
+                email: session.user?.email!,
+            },
+        })
+
+        return NextResponse.json({ data: data }, { status: 201 })
     } catch (err) {
         return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
     }
