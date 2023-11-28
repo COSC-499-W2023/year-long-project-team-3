c428aeac9d36aea2a5845b2cc62aa6a97c308738
te-sa <teresa_saller@freenet.de>
2023-11-27 19:33:22 -0800

    Removed some out-of-scope code from SubmissionBoxCreatePage

--- src/app/submission-box/create/page.tsx
+++ src/app/submission-box/create/page.tsx
@@ -2,8 +2,6 @@
 
 import { useSession } from 'next-auth/react'
 import Header from '@/components/Header'
-import { Box } from '@mui/material'
-import ProgressDots from '@/components/ProgressDots'
 import React from 'react'
 
 // TODO: Implement this page (right now it's only a bare bones version for testing)
@@ -12,26 +10,6 @@ export default function SubmissionBoxCreatePage() {
     return (
         <>
             <Header {...session} />
-            <Box
-                sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    gap: '1rem',
-                    width: '100%',
-                    height: '100%',
-                    padding: '2rem',
-                }}
-            >
-                <Box
-                    sx={{
-                        minWidth: '16rem',
-                        width: '50%',
-                    }}
-                >
-                    <ProgressDots activeStep={2} numSteps={3} labels={['Settings', 'Add Members', 'Create']} />
-                </Box>
-            </Box>
         </>
     )
 }
