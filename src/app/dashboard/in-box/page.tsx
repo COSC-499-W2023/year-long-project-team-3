800ec7f886312db15650446922036cbf512c93c4
Erin Hiebert <eehieb10@telus.net>
2023-11-28 16:35:21 -0800

    Started to connect front page with API to fetch submission boxes

--- src/app/dashboard/in-box/page.tsx
+++ src/app/dashboard/in-box/page.tsx
@@ -5,16 +5,33 @@ import { useSession } from 'next-auth/react'
 import Header from '@/components/Header'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import logger from '@/utils/logger'
 
 export default function SubmissionInBoxPage() {
     const session = useSession()
+    const [submissionInboxes, setSubmissionInboxes] = useState([
+        {
+            id: 123,
+            title: 'null',
+            description: null,
+            createdAt: null,
+            closesAt: null,
+            videoStoreToDate: null,
+            maxVideoLength: null,
+            isPublic: false,
+        },
+    ])
+    const [hasSubmissions, setHasSubmissions] = useState(false)
 
     useEffect(() => {
         async function fetchSubmissionInboxes() {
             const response = await fetch('/api/submission-box/inboxes')
             const submissionInboxes = await response.json()
+            if (submissionInboxes.length > 0) {
+                setSubmissionInboxes(submissionInboxes)
+                setHasSubmissions(true)
+            }
             // TODO: Handle retrieved submission inboxes
         }
 
@@ -25,6 +42,7 @@ export default function SubmissionInBoxPage() {
             })
     }, [])
 
+    console.log(submissionInboxes)
     return (
         <>
             <Header {...session} />
@@ -39,7 +57,17 @@ export default function SubmissionInBoxPage() {
                         sx={{ p: 2, borderRadius: 0.5, height: 1 }}
                         border={1}
                         borderColor={'textSecondary'}
-                    />
+                    >
+                        <ul>
+                            {submissionInboxes.map((submissionBox, id: React.Key) => (
+                                <li key={id}>
+                                    <Box border={1} borderColor={'textSecondary'}>
+                                        <Typography>{submissionBox.title}</Typography>
+                                    </Box>
+                                </li>
+                            ))}
+                        </ul>
+                    </Box>
                 </Box>
             </Box>
         </>
