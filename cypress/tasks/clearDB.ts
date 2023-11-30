9bf32c66b37ea7658ae15a9b7b3435d61fa7184b
justino599 <justin.schoenit@gmail.com>
2023-11-30 13:38:45 -0800

    Update clearDB task

--- cypress/tasks/clearDB.ts
+++ cypress/tasks/clearDB.ts
@@ -1,6 +1,13 @@
 import prisma from '@/lib/prisma'
 
 export default async function clearDB() {
+    await prisma.submittedVideo.deleteMany({})
+    await prisma.requestedSubmission.deleteMany({})
+    await prisma.submissionBoxManager.deleteMany({})
+    await prisma.submissionBox.deleteMany({})
+    await prisma.videoWhitelistedUser.deleteMany({})
+    await prisma.videoWhitelist.deleteMany({})
+    await prisma.video.deleteMany({})
     await prisma.account.deleteMany({})
     await prisma.user.deleteMany({})
 
