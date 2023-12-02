490015296c778daba717cc71ed2e96af16a608a0
te-sa <teresa_saller@freenet.de>
2023-12-01 20:35:02 -0800

    Added logged in context to settings tests

--- cypress/e2e/app/submission-box/create/settings.cy.ts
+++ cypress/e2e/app/submission-box/create/settings.cy.ts
@@ -1,4 +1,5 @@
 import { TIMEOUT } from '../../../../utils/constants'
+import { v4 as uuidv4 } from 'uuid'
 
 describe('Submission box settings tests', () => {
     before(() => {
@@ -7,46 +8,81 @@ describe('Submission box settings tests', () => {
 
     beforeEach(() => {
         cy.task('clearDB')
-        cy.visit('/submission-box')
     })
 
-    it('Should not allow the user to go to the next step without entering a title', () => {
-        // Check that submitting with nothing in the fields presents user with prompts and does not let the user move on
+    context('Logged in', () => {
+        beforeEach(() => {
+            cy.session('testuser', () => {
+                const email = 'user' + uuidv4() + '@example.com'
+                const password = 'Password1'
 
-        cy.wait(1000) // this is necessary because the layout shifts slightly due to the multiline text box
+                // Sign up
+                cy.visit('/signup')
+                cy.get('[data-cy="email"]').type(email)
+                cy.get('[data-cy="password"]').type(password)
+                cy.get('[data-cy="passwordConfirmation"]').type(password)
+                cy.get('[data-cy="submit"]').click()
+                cy.url().should('contain', 'login')
 
-        // Check that the errors do not exist
-        cy.get('p.Mui-error').should('have.length', 0)
+                // Login
+                cy.get('[data-cy=email]').type(email)
+                cy.get('[data-cy=password]').type(password)
+                cy.get('[data-cy=submit]').click()
+                cy.url().should('not.contain', 'login')
 
-        cy.get('[data-cy="next"]').click()
+                // Navigate to Create New Submission Box
+                cy.get('[data-cy=submission-box-create]')
+            })
+            cy.visit('/submission-box/create')
+        })
+        it('Should not allow the user to go to the next step without entering a title', () => {
+            // Check that submitting with nothing in the fields presents user with prompts and does not let the user move on
 
-        cy.url().should('include', '/submission-box')
+            cy.wait(1000) // this is necessary because the layout shifts slightly due to the multiline text box
 
-        cy.get('p.Mui-error').should('have.length', 1)
-        cy.get('[data-cy="submission-box-title"]')
-            .find('p.Mui-error')
-            .should('be.visible')
-            .and('contain', 'Please enter a title for your submission box')
+            // Check that the errors do not exist
+            cy.get('p.Mui-error').should('have.length', 0)
+
+            cy.get('[data-cy="next"]').click()
+
+            cy.url().should('include', '/submission-box')
+
+            cy.get('p.Mui-error').should('have.length', 1)
+            cy.get('[data-cy="submission-box-title"]')
+                .find('p.Mui-error')
+                .should('be.visible')
+                .and('contain', 'Please enter a title for your submission box')
+        })
+
+        it('Should allow user to click next', () => {
+            // User data
+            const title = 'My Test Title'
+
+            // Create submission box
+            cy.get('[data-cy="submission-box-title"]').type(title)
+
+            // Double click is a known issue, cypress is not acting correctly on browser preview deployment
+            cy.get('[data-cy="next"]').click().click()
+
+            // We shouldn't be on the seeing the settings step anymore
+            cy.get('[data-cy="title"]', { timeout: TIMEOUT.EXTRA_LONG }).contains('Request Submissions')
+        })
+
+        it.skip('Should let the user return to the previous page using the return to dashboard button', () => {
+            cy.get('[data-cy="back"]').click()
+
+            // TODO: change this to test for appropriate URL
+            cy.url().should('include', '/dashboard')
+        })
     })
 
-    it('Should allow user to click next', () => {
-        // User data
-        const title = 'My Test Title'
+    context('Not logged in', () => {
+        beforeEach(() => {
+            cy.visit('/submission-box/create')
+        })
 
-        // Create submission box
-        cy.get('[data-cy="submission-box-title"]').type(title)
-
-        // Double click is a known issue, cypress is not acting correctly on browser preview deployment
-        cy.get('[data-cy="next"]').click().click()
-
-        // We shouldn't be on the seeing the settings step anymore
-        cy.get('[data-cy="title"]', { timeout: TIMEOUT.EXTRA_LONG }).contains('Request Submissions')
-    })
-
-    it.skip('Should let the user return to the previous page using the return to dashboard button', () => {
-        cy.get('[data-cy="back"]').click()
-
-        // TODO: change this to test for appropriate URL (currently not implemented as the user is not logged in for this test and would therefore be re-routed to login)
-        // cy.url().should('include', '/dashboard')
+        it('Should redirect to login', () => {
+            cy.url().should('contain', 'login')
+        })
     })
 })
