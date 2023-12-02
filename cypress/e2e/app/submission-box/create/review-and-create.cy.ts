76e62c5f29482ff0c06925100aa8ca0fba36e405
te-sa <teresa_saller@freenet.de>
2023-12-01 21:06:57 -0800

    Added context to review and create page and changed theme

--- cypress/e2e/app/submission-box/create/review-and-create.cy.ts
+++ cypress/e2e/app/submission-box/create/review-and-create.cy.ts
@@ -1,4 +1,5 @@
 import { TIMEOUT } from '../../../../utils/constants'
+import { v4 as uuidv4 } from 'uuid'
 
 describe('Submission box review and create tests', () => {
     before(() => {
@@ -7,61 +8,82 @@ describe('Submission box review and create tests', () => {
 
     beforeEach(() => {
         cy.task('clearDB')
-        cy.visit('/submission-box')
     })
 
-    it('Should allow the user to review their submission box before creating', () => {
-        const title = 'My Test Title'
+    context('Logged in', () => {
+        beforeEach(() => {
+            cy.session('testuser', () => {
+                const email = 'user' + uuidv4() + '@example.com'
+                const password = 'Password1'
 
-        // Create submission box
-        cy.get('[data-cy="submission-box-title"]').type(title)
+                // Sign up
+                cy.visit('/signup')
+                cy.get('[data-cy="email"]').type(email)
+                cy.get('[data-cy="password"]').type(password)
+                cy.get('[data-cy="passwordConfirmation"]').type(password)
+                cy.get('[data-cy="submit"]').click()
+                cy.url().should('contain', 'login')
 
-        // Double click is a known issue, cypress is not acting correctly on browser preview deployment
-        cy.get('[data-cy="next"]').click().click()
+                // Login
+                cy.get('[data-cy=email]').type(email)
+                cy.get('[data-cy=password]').type(password)
+                cy.get('[data-cy=submit]').click()
+                cy.url().should('not.contain', 'login')
+            })
 
-        // We should be on the request submission step
-        cy.get('[data-cy="title"]', { timeout: TIMEOUT.EXTRA_LONG }).contains('Request Submissions')
+            cy.visit('/dashboard')
+            cy.get('[data-cy="Create new"]').click()
 
-        cy.get('[data-cy="next"]').click()
+            const title = 'My Test Title'
 
-        cy.get('[data-cy="title"]', { timeout: TIMEOUT.EXTRA_LONG }).contains('Review & Create')
+            // Create submission box
+            cy.get('[data-cy="submission-box-title"]').type(title)
 
-        // TODO: extend this test to check more details about the page
+            // Double click is a known issue, cypress is not acting correctly on browser preview deployment
+            cy.get('[data-cy="next"]').click().click()
+
+            // We should be on the request submission step
+            cy.get('[data-cy="title"]', { timeout: TIMEOUT.EXTRA_LONG }).contains('Request Submissions')
+
+            cy.get('[data-cy="next"]').click()
+        })
+
+        it('Should allow the user to review their submission box before creating', () => {
+            cy.get('[data-cy="title"]', { timeout: TIMEOUT.EXTRA_LONG }).contains('Review & Create')
+
+            // TODO: extend this test to check more details about the page
+        })
+
+        it.skip('Should let the user return to the previous page using the return to dashboard button', () => {
+            cy.get('[data-cy="back-button"]').click()
+
+            // TODO: change this to test for appropriate URL
+            cy.url().should('include', '/dashboard')
+        })
+
+        it('Should let the user return to the previous step using the back button', () => {
+            cy.get('[data-cy="back"]').click()
+
+            cy.url().should('include', '/submission-box')
+
+            cy.get('[data-cy="title"]', { timeout: TIMEOUT.EXTRA_LONG }).contains('Request Submissions')
+        })
+
+        it.skip('Should allow user to click create', () => {
+            cy.get('[data-cy="next"]').click()
+
+            cy.url().should('include', '/dashboard')
+        })
     })
 
-    it.skip('Should let the user return to the previous page using the return to dashboard button', () => {
-        cy.get('[data-cy="back-button"]').click()
+    context('Not logged in', () => {
+        beforeEach(() => {
+            cy.visit('/submission-box/create')
+        })
 
-        // TODO: change this to test for appropriate URL (currently not implemented as the user is not logged in for this test and would therefore be re-routed to login)
-        // cy.url().should('include', '/dashboard')
-    })
-
-    it('Should let the user return to the previous step using the back button', () => {
-        const title = 'My Test Title'
-
-        // Create submission box
-        cy.get('[data-cy="submission-box-title"]').type(title)
-
-        // Double click is a known issue, cypress is not acting correctly on browser preview deployment
-        cy.get('[data-cy="next"]').click().click()
-
-        // We should be on the request submission step
-        cy.get('[data-cy="title"]', { timeout: TIMEOUT.EXTRA_LONG }).contains('Request Submissions')
-
-        cy.get('[data-cy="next"]').click()
-
-        cy.get('[data-cy="title"]', { timeout: TIMEOUT.EXTRA_LONG }).contains('Review & Create')
-
-        cy.get('[data-cy="back"]').click()
-
-        cy.url().should('include', '/submission-box')
-
-        cy.get('[data-cy="title"]', { timeout: TIMEOUT.EXTRA_LONG }).contains('Request Submissions')
-    })
-
-    it.skip('Should allow user to click create', () => {
-        cy.get('[data-cy="next"]').click()
-
-        cy.url().should('include', '/dashboard')
+        // TODO: awaiting middleware changes
+        it.skip('Should redirect to login', () => {
+            cy.url().should('contain', 'login')
+        })
     })
 })
