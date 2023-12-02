5bdc8ca4e1ccfdad9a5f49ae42f7612f82e6f4f1
Hedgemon4 <sethlinc4@gmail.com>
2023-12-01 20:13:45 -0800

    Started adding component to indicate that the video is processing

--- src/app/video/edit/[videoId]/page.tsx
+++ src/app/video/edit/[videoId]/page.tsx
@@ -1,5 +1,4 @@
 'use client'
-import type { InferGetStaticPropsType, GetStaticProps, GetStaticPaths } from 'next'
 import React, { useEffect, useState } from 'react'
 import Header from '@/components/Header'
 import { SessionContextValue, useSession } from 'next-auth/react'
---
+++ src/components/VideoProcessing/index.tsx
@@ -0,0 +1,9 @@
+import { Typography } from '@mui/material'
+
+export default function VideoProcessing() {
+    return (
+        <>
+            <Typography>Your Video is Currently Being Processed. Please wait or come back later.</Typography>
+        </>
+    )
+}
