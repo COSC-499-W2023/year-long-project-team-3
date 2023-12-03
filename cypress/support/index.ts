b918e6e24ae05a703150a2bbbdf71e83f9ca4aee
justino599 <justin.schoenit@gmail.com>
2023-12-02 21:31:43 -0800

    Cleanup broken code

--- cypress/support/index.ts
+++
@@ -1,7 +0,0 @@
-// File defines custom commands into the Cypress namespace
-declare namespace Cypress {
-    interface Chainable {
-        dataCy(value: string):  Cypress.Chainable<JQuery<HTMLElement>>
-        typeDatePicker(selector: string, value: string):  Cypress.Chainable<JQuery<HTMLElement>>
-    }
-}
