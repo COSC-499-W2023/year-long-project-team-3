eb48e49c981d1b63124bcd9d536b2c26e7c72e4e
justino599 <justin.schoenit@gmail.com>
2023-12-02 21:11:07 -0800

    Could it be this simple?

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -42,17 +42,17 @@ Cypress.Commands.add('dataCy', (value: string) => {
 
 Cypress.Commands.add('typeDatePicker', (selector: string, value: string) => {
     cy.get('body').then(($body) => {
-        const mobilePickerSelector = `${ selector } input[readonly]`
-        const isMobile = $body.find(mobilePickerSelector).length > 0
-        if (isMobile) {
-            // The MobileDatePicker component has readonly inputs and needs to
-            // be opened and clicked on edit so its inputs can be edited
-            cy.get(mobilePickerSelector).click()
-            cy.get('[role="dialog"] [aria-label="calendar view is open, go to text input view"]').click()
-            cy.get(`[role="dialog"] ${ selector }`).find('input').clear().type(value)
-            cy.contains('[role="dialog"] button', 'OK').click()
-        } else {
-            cy.get(selector).find('input').clear().type(value)
-        }
+        // const mobilePickerSelector = `${ selector } input[readonly]`
+        // const isMobile = $body.find(mobilePickerSelector).length > 0
+        // if (isMobile) {
+        //     // The MobileDatePicker component has readonly inputs and needs to
+        //     // be opened and clicked on edit so its inputs can be edited
+        //     cy.get(mobilePickerSelector).click()
+        //     cy.get('[role="dialog"] [aria-label="calendar view is open, go to text input view"]').click()
+        //     cy.get(`[role="dialog"] ${ selector }`).find('input').clear().type(value)
+        //     cy.contains('[role="dialog"] button', 'OK').click()
+        // } else {
+        cy.get(selector).find('input', { force: true }).clear().type(value)
+        // }
     })
 })
