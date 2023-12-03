48b70df1030c0d6d628bff59f8644adf3e2446e4
Hedgemon4 <sethlinc4@gmail.com>
2023-12-02 22:04:42 -0800

    Hopefully fixed issue

--- cypress/tasks/getLatestVideo.ts
+++ cypress/tasks/getLatestVideo.ts
@@ -1,9 +1,12 @@
 import { Video } from '@prisma/client'
+import prisma from '@/lib/prisma'
 
-export default async function getLatestVideo(): Promise<Video> {
-    return await prisma.video.findFirst({
+export default async function getLatestVideo(): Promise<Video | null> {
+    const video = prisma.video.findFirst({
         orderBy: {
             createdAt: 'desc',
         },
     })
+    if (video === null) {throw Error('Prisma found no video and it should have')}
+    return video
 }
