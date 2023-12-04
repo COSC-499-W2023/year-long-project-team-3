2cd59847c511d13ce93769f2c39fc990204a5fbc
justino599 <justin.schoenit@gmail.com>
2023-12-03 20:48:56 -0800

    Week 13 Team Log

--- cypress/e2e/app/video/upload.cy.ts
+++ cypress/e2e/app/video/upload.cy.ts
@@ -54,7 +54,7 @@ describe('Test Video Upload and Streaming Processing Pipeline', () => {
             })
 
             /* Check that the processed video preview is displayed once we redirect to the edit page */
-            cy.get('[data-cy=video-player]', { timeout: TIMEOUT.EXTRA_EXTRA_LONG }).should('be.visible')
+            cy.get('[data-cy=video-player]', { timeout: 100000000 }).should('be.visible')
         })
     })
 })
