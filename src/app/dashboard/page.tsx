200d4d1a7e286aea7ec797e6f7e97e5b74cd9bfd
ketphan02 <tuankietvn@outlook.com>
2023-12-18 01:28:20 -0800

    Clean up

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -151,7 +151,6 @@ export default function DashboardPage() {
                         )}
                     </Box>
                 </Box>
-                ; ;
             </Box>
         </>
     )
