456637d025fc805cfee26da0e2ae11755bf3413d
justino599 <justin.schoenit@gmail.com>
2024-01-15 23:00:08 -0800

    Google accounts have verified email addresses by default

--- src/app/api/verify-email/is-verified/route.ts
+++ src/app/api/verify-email/is-verified/route.ts
@@ -20,9 +20,31 @@ export async function GET(req: NextRequest) {
             },
             select: {
                 emailVerified: true,
+                createdAt: true,
+                accounts: {
+                    select: {
+                        provider: true,
+                    },
+                },
             },
         })
-        return NextResponse.json({ isVerified: !!user.emailVerified }, { status: 200 })
+
+        if (!!user.emailVerified) {
+            return NextResponse.json({ isVerified: true }, { status: 200 })
+        } else if (user.accounts.some(account => account.provider === 'google')) {
+            // Update user record to show that their email is verified if they log in with google
+            await prisma.user.update({
+                where: {
+                    email: session.user.email,
+                },
+                data: {
+                    emailVerified: user.createdAt,
+                },
+            })
+            return NextResponse.json({ isVerified: true }, { status: 200 })
+        } else {
+            return NextResponse.json({ isVerified: false }, { status: 200 })
+        }
     } catch (e) {
         return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
     }
