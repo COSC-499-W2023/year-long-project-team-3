939920215554d1b91c1a1ec310877a3be9a457f5
justino599 <justin.schoenit@gmail.com>
2024-01-15 19:39:22 -0800

    We CAN fetch

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,28 @@
 import { withAuth } from 'next-auth/middleware'
 import { NextRequest, NextResponse } from 'next/server'
 import logger from '@/utils/logger'
+import { getServerSession } from 'next-auth'
 
 export default withAuth(
-    function middleware(request: NextRequest) {
+    async function middleware(request: NextRequest) {
+        console.log('Hello there')
+
+        const cookies = request.headers.get('cookie')
+        const requestHeaders: HeadersInit = new Headers()
+        cookies?.split(';').map(cookie => {
+            const [key, value] = cookie.trim().split('=')
+            requestHeaders.set(key, value)
+        })
+        requestHeaders.forEach((a, b) => {
+            console.log(`COOKIE: ${ a } = ${ b }`)
+        })
+
+        const res = await (await fetch(process.env.NEXT_PUBLIC_BASE_URL + '/api/verify-email/is-verified', {
+            method: 'GET',
+            headers: requestHeaders,
+        })).json()
+        console.log(res)
+
         if (request.nextUrl.pathname.startsWith('/api')) {
             logger.info(`API request: ${ request.nextUrl.pathname }`)
             return NextResponse.next()
