53bc879641b65e553485796a2e8ec3e6fe85de5e
justino599 <justin.schoenit@gmail.com>
2024-01-15 22:26:36 -0800

    Check if email is verified

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,38 +1,39 @@
 import { withAuth } from 'next-auth/middleware'
 import { NextRequest, NextResponse } from 'next/server'
 import logger from '@/utils/logger'
-import { getServerSession } from 'next-auth'
 
 export default withAuth(
     async function middleware(request: NextRequest) {
-        console.log('Hello there')
-
-        const cookies = request.headers.get('cookie')
-        const requestHeaders: HeadersInit = new Headers()
-        cookies?.split(';').map(cookie => {
-            const [key, value] = cookie.trim().split('=')
-            requestHeaders.set(key, value)
-        })
-        requestHeaders.forEach((a, b) => {
-            console.log(`COOKIE: ${ a } = ${ b }`)
-        })
-
-        const res = await (await fetch(process.env.NEXT_PUBLIC_BASE_URL + '/api/verify-email/is-verified', {
-            method: 'GET',
-            headers: requestHeaders,
-        })).json()
-        console.log(res)
+        // Check if we should redirect when a user's email isn't verified
+        const cookies = request.cookies
+        const isLoggedIn = cookies.has('next-auth.session-token')
+        const protectedPages = ['/dashboard', '/submission-box', '/video', '/api/video', '/api/submission-box', '/api/videos']
+        if (
+            isLoggedIn &&
+            protectedPages.some(page => request.nextUrl.pathname.startsWith(page))
+        ) {
+            const res = await (await fetch(process.env.NEXT_PUBLIC_BASE_URL + '/api/verify-email/is-verified', {
+                method: 'GET',
+                headers: {
+                    'cookie': cookies.toString(),
+                },
+            })).json()
+            if (!(res.isVerified ?? false)) {
+                const url = request.nextUrl.clone()
+                url.pathname = '/verify-email'
+                return NextResponse.redirect(url)
+            }
+        }
 
         if (request.nextUrl.pathname.startsWith('/api')) {
             logger.info(`API request: ${ request.nextUrl.pathname }`)
-            return NextResponse.next()
         }
         return NextResponse.next()
     },
     {
         callbacks: {
             authorized: ({ req, token }) => {
-                const protectedPages = ['/dashboard', '/submission-box', '/video', '/api/video']
+                const protectedPages = ['/dashboard', '/submission-box', '/video', '/api/video', '/api/submission-box', '/api/videos']
                 return !(token === null && protectedPages.some((page) => req.nextUrl.pathname.startsWith(page)))
             },
         },
