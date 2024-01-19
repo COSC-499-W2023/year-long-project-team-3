411911c8e402b78ee48f5a732c6f8d341017bca8
Seth Akins <sethlinc4@gmail.com>
2024-01-18 20:28:08 -0800

    Addressed the changes requested on my PR by K to fix the setup file

--- next.config.js
+++ next.config.js
@@ -1,5 +1,23 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = () => {
+    const environmentVariables = [
+        'NEXT_PUBLIC_BASE_URL',
+        'NEXT_IMAGE_WHITELIST_HOSTNAMES',
+        'DATABASE_URL',
+        'NEXT_PUBLIC_GOOGLE_CLIENT_ID',
+        'NEXT_PUBLIC_GOOGLE_CLIENT_SECRET',
+        'NEXT_PUBLIC_NEXTAUTH_SECRET',
+        'AWS_UPLOAD_BUCKET',
+        'AWS_UPLOAD_REGION',
+    ]
+
+    // Check if all environment variables are set
+    const missingVariables = environmentVariables.filter((variable) => !process.env[variable])
+    if (missingVariables.length > 0) {
+        const errorMessage = `The environment variables are missing: ${ missingVariables.join(', ') }`
+        throw new ReferenceError(errorMessage)
+    }
+
     return {
         images: {
             domains: process.env.NEXT_IMAGE_WHITELIST_HOSTNAMES.split(' '),
