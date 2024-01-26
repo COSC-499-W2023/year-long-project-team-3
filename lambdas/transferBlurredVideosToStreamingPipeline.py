a6c73f0737adb579f94d434b8ec1d3cc81982d65
te-sa <teresa_saller@freenet.de>
2024-01-25 18:47:22 -0800

    Fixed file name issue

--- lambdas/transferBlurredVideosToStreamingPipeline.py
+++ lambdas/transferBlurredVideosToStreamingPipeline.py
@@ -25,15 +25,17 @@ def lambda_handler(event, context):
 
     print(f"Source Bucket: {source_bucket}, Key: {key}, Destination Bucket: {destination_bucket}")
 
-    # Define a regular expression pattern to capture the substring between the second underscore and the .mp4
-    pattern = r'_(.*?)_(.*?)\.mp4'
+    # Define a regular expression pattern to capture the substring starting with the last underscore (this will contain .mp4 or .mov as well)
+    # We need to do this because the user's filename may contain underscores
+    pattern = r'_([^_]+)$'
 
     # Use re.search to find the match in the filename
     match = re.search(pattern, key)
 
     # Check if a match is found, and extract the desired string
     if match:
-        extracted_string = match.group(2)
+        # Remove the file extension (.mp4 or .mov) if a match was found and treat that as the extracted_string
+        extracted_string = match.group(1)[:-4]
         logger.info(extracted_string)
     else:
         raise PatternNotFoundException("Pattern not found in the filename.")
