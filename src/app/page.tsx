3743850028a22fac5c18ba286855f4e762246457
justino599 <justin.schoenit@gmail.com>
2024-01-26 15:45:59 -0800

    Test if it works

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,8 @@ import LandingPageButton from '@/components/LandingPageButton'
 import { type SessionContextValue, useSession } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
 
+// PEEPORIOT
+
 export default function HomePage() {
     const session: SessionContextValue = useSession()
     const router = useRouter()
