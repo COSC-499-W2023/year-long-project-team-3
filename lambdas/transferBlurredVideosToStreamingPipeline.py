d3430b944698698ef67b81f010dc1c42e52233a9
te-sa <teresa_saller@freenet.de>
2024-02-08 20:01:16 -0800

    Rewrote lambda and removed spaces from video titles

--- lambdas/transferBlurredVideosToStreamingPipeline.py
+++ lambdas/transferBlurredVideosToStreamingPipeline.py
@@ -5,28 +5,34 @@ import os
 import re
 import logging
 
+
 class PatternNotFoundException(Exception):
     pass
+
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-s3 = boto3.resource('s3')
 
 def lambda_handler(event, context):
     logger.info("New files uploaded to the source bucket.")
 
-    key = event['Records'][0]['s3']['object']['key']
+    # Create a Boto3 S3 resource
+    s3_resource = boto3.resource('s3')
 
+    # Get source bucket and key from event, destination bucket from env variables
     source_bucket = event['Records'][0]['s3']['bucket']['name']
+    key = event['Records'][0]['s3']['object']['key']
     destination_bucket = os.environ['destination_bucket']
 
-    source = {'Bucket': source_bucket, 'Key': key}
+    # Instantiate the ObjectWrapper class with the S3 object
+    s3_object = s3_resource.Object(source_bucket, key)
+    object_wrapper = ObjectWrapper(s3_object)
 
     print(f"Source Bucket: {source_bucket}, Key: {key}, Destination Bucket: {destination_bucket}")
 
-    # Define a regular expression pattern to capture the substring starting with the last underscore (this will contain .mp4 or .mov as well)
-    # We need to do this because the user's filename may contain underscores
+    # Define a regular expression pattern to capture the substring starting with the last underscore (this will
+    # contain .mp4 or .mov as well) We need to do this because the user's filename may contain underscores
     pattern = r'_([^_]+)$'
 
     # Use re.search to find the match in the filename
@@ -41,19 +47,60 @@ def lambda_handler(event, context):
         raise PatternNotFoundException("Pattern not found in the filename.")
 
     try:
-        response = s3.meta.client.copy(source, destination_bucket, key)
+        dest_s3_object = s3_resource.Object(destination_bucket, key)
+
+        # Copy the S3 object to another bucket
+        object_wrapper.copy(dest_s3_object)
         logger.info("File copied to the destination bucket successfully!")
 
         # Create json from video name, deposit json once video has been successfully uploaded
         metadata_file = {'videoId': extracted_string, 'srcVideo': key}
         logger.info(metadata_file)
         upload_byte_stream = bytes(json.dumps(metadata_file), 'utf-8')
-        s3.meta.client.put_object(Bucket=destination_bucket, Key=extracted_string + '.json', Body=upload_byte_stream)
+        s3_resource.meta.client.put_object(Bucket=destination_bucket, Key=extracted_string + '.json',
+                                           Body=upload_byte_stream)
         logger.info("Metadata added to destination bucket successfully!")
-    except botocore.exceptions.ClientError as error:
-        logger.error("There was an error copying the file to the destination bucket")
-        print('Error Message: {}'.format(error))
+    except Exception as e:
+        # Handle exceptions or errors
+        logger.error(f"Error: {e}")
 
-    except botocore.exceptions.ParamValidationError as error:
-        logger.error("Missing required parameters while calling the API.")
-        print('Error Message: {}'.format(error))
+
+class ObjectWrapper:
+    """Encapsulates S3 object actions."""
+
+    def __init__(self, s3_object):
+        """
+        :param s3_object: A Boto3 Object resource. This is a high-level resource in Boto3
+                          that wraps object actions in a class-like structure.
+        """
+        self.object = s3_object
+        self.key = self.object.key
+
+    def copy(self, dest_object):
+        """
+        Copies the object to another bucket.
+
+        :param dest_object: The destination object initialized with a bucket and key.
+                            This is a Boto3 Object resource.
+        """
+        try:
+            dest_object.copy_from(
+                CopySource={"Bucket": self.object.bucket_name, "Key": self.object.key}
+            )
+            dest_object.wait_until_exists()
+            logger.info(
+                "Copied object from %s:%s to %s:%s.",
+                self.object.bucket_name,
+                self.object.key,
+                dest_object.bucket_name,
+                dest_object.key,
+            )
+        except Exception:
+            logger.exception(
+                "Couldn't copy object from %s/%s to %s/%s.",
+                self.object.bucket_name,
+                self.object.key,
+                dest_object.bucket_name,
+                dest_object.key,
+            )
+            raise
