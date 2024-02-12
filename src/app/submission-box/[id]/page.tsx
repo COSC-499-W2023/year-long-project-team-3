7fbdcbba25c28d110194a8fb399497f168bf895c
Erin Hiebert <eehieb10@telus.net>
2024-02-11 19:08:13 -0800

    redo changes

--- src/app/submission-box/[id]/page.tsx
+++ src/app/submission-box/[id]/page.tsx
@@ -1,9 +1,7 @@
 'use client'
 
 import { usePathname, useRouter } from 'next/navigation'
-import Header from '@/components/Header'
 import React, { useEffect, useState } from 'react'
-import { SessionContextValue, useSession } from 'next-auth/react'
 import { Typography, Box, Button, Link } from '@mui/material'
 import { SubmissionBox, Video } from '@prisma/client'
 import VideoList from '@/components/VideoList'
@@ -43,7 +41,6 @@ export default function SubmissionBoxDetailPage() {
         })
     }, [boxId, router])
 
-    // @ts-ignore
     return (
         <>
             <Box height='100wv' width='100%'>
