87163e47e9b6e1a949f9c5802de8d5194c60acf6
te-sa <teresa_saller@freenet.de>
2024-02-18 17:38:03 -0800

    Styling card

--- src/components/VideoCard/index.tsx
+++ src/components/VideoCard/index.tsx
@@ -15,9 +15,9 @@ export default function VideoCard(props: VideoCardProps) {
     const router = useRouter()
 
     return (
-        <Card sx={{ display: 'flex', width: '100%', height: 200, mb: 2 }}>
+        <Card sx={{ display: 'flex', width: '100%', height: 200, mb: 2, px: 1 }}>
             {!!props.thumbnailUrl && (
-                <Box display={'flex'} alignItems={'center'} maxWidth={300} paddingLeft={2}>
+                <Box display={'flex'} alignItems={'center'} maxWidth={300} minWidth={300} paddingLeft={2}>
                     <CardMedia
                         component='img'
                         src={props.thumbnailUrl}
@@ -29,11 +29,11 @@ export default function VideoCard(props: VideoCardProps) {
             <Box sx={{width: 40}}></Box>
             <CardContent sx={{display: 'flex', width: '100%', flexDirection: 'column', justifyContent: 'space-between'}}>
                 <Box>
-                    <Box sx={{display: 'flex', justifyContent: 'space-between',  width: '100%', mb: 2}}>
-                        <Typography variant='h5' color={theme.palette.secondary.main} sx={{ fontWeight: 'bold'}}>{props.title}</Typography>
-                        <Typography sx={{whiteSpace: 'nowrap'}}>Uploaded on: {getDateString()}</Typography>
+                    <Box sx={{display: 'flex', justifyContent: 'space-between', width: '100%', mb: 2}}>
+                        <Typography noWrap variant='h5' color={theme.palette.secondary.main} sx={{ fontWeight: 'bold', width: '48rem'}}>{props.title}</Typography>
+                        <Typography noWrap>Uploaded on: {getDateString()}</Typography>
                     </Box>
-                    <Typography>{props.description}</Typography>
+                    <Typography sx={{ maxHeight: '4.5rem', overflow: 'hidden'}}>{props.description}</Typography>
                 </Box>
                 {props.isSubmitted? <Typography>Submitted to: </Typography>:<Typography color={theme.palette.secondary.main} sx={{ fontWeight: 600}}>Not Submitted</Typography>}
             </CardContent>
