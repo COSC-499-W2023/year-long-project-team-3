c64030c5cc1efafc08a89017333d08bb71e8f1f9
Seth Akins <sethlinc4@gmail.com>
2024-03-09 14:35:37 -0800

    Changed height of loading icon

--- src/components/PageLoadProgress/index.tsx
+++ src/components/PageLoadProgress/index.tsx
@@ -3,6 +3,7 @@ import { Box, CircularProgress } from '@mui/material'
 export default function PageLoadProgress() {
     return (
         <Box
+            height='80vh',
             display='flex'
             justifyContent='center'
             alignItems='center'
