47c689714c2493ab9e07b5036cf6bdff5136f285
Erin Hiebert <eehieb10@telus.net>
2024-03-09 13:00:45 -0800

    Documented the update api

--- src/app/api/submission-box/update/[id]/route.ts
+++ src/app/api/submission-box/update/[id]/route.ts
@@ -4,32 +4,38 @@ import prisma from '@/lib/prisma'
 import logger from '@/utils/logger'
 
 export async function PUT(req: NextRequest): Promise<NextResponse> {
+    // This api works as the backend for updating a submission boxes title, description and closing date in the database
     try {
         const session = await getServerSession()
         if (!session || !session.user?.email) {
             return NextResponse.json({ error: 'You must be signed in to edit a submission box' }, { status: 401 })
         }
 
+        // Get the specific submission box
         const submissionBoxId = req.nextUrl.pathname.split('/').pop()
         if (!submissionBoxId) {
             return NextResponse.json({ error: 'No submission box id provided' }, { status: 400 })
         }
 
+        // Unpack the form information that the user inputted
         const submissionFormBody = await req.formData()
         const title = submissionFormBody.get('title')
         const description = submissionFormBody.get('description')
         const closesAt = submissionFormBody.get('closesAt')
 
+        // Determine if the title is a valid string and is not empty
         if (!title || typeof title !== 'string' || title === '') {
             logger.error(`User ${ session.user.email } did not provide a title`)
             return NextResponse.json({ error: 'No title provided' }, { status: 500 })
         }
 
+        // Determine if the description is a valid string or is empty
         if (description !== null && typeof description !== 'string') {
             logger.error('Unexpected description type')
             return NextResponse.json({ error: 'Unexpected description type' }, { status: 500 })
         }
 
+        // If there is a closing date, determine if the date is not already past
         if (!!closesAt && new Date(closesAt.toString()) < new Date()) {
             logger.error('Date chosen has already past')
             return NextResponse.json({ error: 'Date chosen has already past' }, { status: 500 })
@@ -47,6 +53,7 @@ export async function PUT(req: NextRequest): Promise<NextResponse> {
             })
         ).id
 
+        // Get the submission box being modifyed in the database
         const ownedSubmissionBox = await prisma.submissionBoxManager.findUniqueOrThrow({
             where: {
                 // eslint-disable-next-line camelcase
@@ -60,11 +67,13 @@ export async function PUT(req: NextRequest): Promise<NextResponse> {
             },
         })
 
+        // Determine if user has permission to modify submission details
         if (ownedSubmissionBox.viewPermission !== 'owner') {
             logger.error(`User ${ userId } does not have permission to modify submission box ${ submissionBoxId }`)
             return NextResponse.json({ error: 'Forbidden' }, { status: 403 })
         }
 
+        // Finally, update the fields in the database
         const updatedSubmissionBox = await prisma.submissionBox.update({
             where: {
                 id: submissionBoxId,
