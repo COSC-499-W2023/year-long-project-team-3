d6e688f9d7983f209d9bf8b1dd4b7b2af6668c6c
Erin Hiebert <eehieb10@telus.net>
2024-03-10 12:21:14 -0700

    Addressed requested changes for tests

--- cypress/e2e/app/submission-box/detail/editBoxDetail.cy.ts
+++ cypress/e2e/app/submission-box/detail/editBoxDetail.cy.ts
@@ -1,24 +1,22 @@
 import { TIMEOUT } from '../../../../utils/constants'
-import runWithRetry from '../../../../utils/runUntilExist'
 
 describe('Recieving Dashboard Details Page Tests', () => {
-    // User information
-    const email = 'modification@detail.test'
-    const password = 'Pass1234'
     // Submission Box information
-    const emptySubmissionBoxTitle = 'Test Modification with only title'
-    const fullSubmissionBoxTitle = 'Test Modification with all fields'
+    const submissionBoxTitle1 = 'Test Modification with only title'
+    const submissionBoxTitle2 = 'Test Modification with all fields'
     const submissionBoxDescription = 'Description for a submission box with all fields filled out'
     const submissionBoxTime = new Date('December 17, 2045 03:24:00')
-    const submissionBoxTimeString = '2045/12/17 03:24 AM'
+    const submissionBoxTimeString = '2045/12/17 03:24 AM'   // How textfield displays the date information
     // New values for editing
     const newTitle = 'A new title for the submission box'
     const newDescription = 'A new description for the submission box.'
-    const newTimeCharacters = '2060061313:30AM'
+    const newTimeCharacters = '2060 06 13 03:30 AM' // Characters to be typed in DatePicker
     const newTimeDisplay = '2060/06/13 03:30 AM'
-    const invalidTimeCharacters = '1995042312:00PM'
     beforeEach(() => {
         cy.task('clearDB')
+        // User information
+        const email = 'modification@detail.test'
+        const password = 'Pass1234'
         // Create a user with two managed submission boxes, one with only title, the other with all fields.
         cy.task('createUser', { email, password })
         cy.visit('/login')
@@ -27,8 +25,8 @@ describe('Recieving Dashboard Details Page Tests', () => {
         cy.get('[data-cy=submit]').click()
         cy.url({ timeout: TIMEOUT.EXTRA_LONG }).should('not.contain', 'login')
         cy.task('getUserId', email).then((userId) => {
-            cy.task('createSubmissionBoxForSubmissions', { submissionBoxTitle: emptySubmissionBoxTitle, userId })
-            cy.task('createSubmissionBoxForSubmissions', { submissionBoxTitle: fullSubmissionBoxTitle, submissionBoxDescription, userId, closesAt: submissionBoxTime })
+            cy.task('createSubmissionBoxForSubmissions', { submissionBoxTitle: submissionBoxTitle1, userId })
+            cy.task('createSubmissionBoxForSubmissions', { submissionBoxTitle: submissionBoxTitle2, submissionBoxDescription, userId, closesAt: submissionBoxTime })
         })
         cy.reload()
         cy.visit('/dashboard')
@@ -39,17 +37,17 @@ describe('Recieving Dashboard Details Page Tests', () => {
         cy.wait(1000)
         cy.get('[data-cy="Manage Boxes"]').click()
 
-        cy.get(`[data-cy="${ emptySubmissionBoxTitle }"]`)
+        cy.get(`[data-cy="${ submissionBoxTitle1 }"]`)
         cy.wait(1000)   // Waiting for it to be clickable
-        cy.get(`[data-cy="${ emptySubmissionBoxTitle }"]`).click()
+        cy.get(`[data-cy="${ submissionBoxTitle1 }"]`).click()
 
-        cy.get('[data-cy="submissionBoxTitle"]').should('contain', emptySubmissionBoxTitle)
+        cy.get('[data-cy="submissionBoxTitle"]').should('contain', submissionBoxTitle1)
         // Start the editing process
         cy.get('[data-cy="edit-icon"]').should('be.visible').click()
         // Determine that the title field is visible
         cy.get('[data-cy="submissionBoxTitleEditing"]').should('be.visible').click()
         // Focus the textfield, determine that it has the correct current value
-        cy.get('[data-cy="submissionBoxTitleEditing"]').focused().should('have.value', emptySubmissionBoxTitle)
+        cy.get('[data-cy="submissionBoxTitleEditing"]').focused().should('have.value', submissionBoxTitle1)
         // Focus and clear the textfield and type in new title
         cy.get('[data-cy="submissionBoxTitleEditing"]').focused().clear().type(newTitle)
         cy.get('[data-cy="updateButton"]').should('be.visible').click()
@@ -63,18 +61,18 @@ describe('Recieving Dashboard Details Page Tests', () => {
         cy.wait(1000)
         cy.get('[data-cy="Manage Boxes"]').click()
 
-        cy.get(`[data-cy="${ fullSubmissionBoxTitle }"]`)
+        cy.get(`[data-cy="${ submissionBoxTitle2 }"]`)
         cy.wait(1000)   // Waiting for it to be clickable
-        cy.get(`[data-cy="${ fullSubmissionBoxTitle }"]`).click()
+        cy.get(`[data-cy="${ submissionBoxTitle2 }"]`).click()
 
-        cy.get('[data-cy="submissionBoxTitle"]', { timeout: TIMEOUT.EXTRA_LONG }).should('contain', fullSubmissionBoxTitle)
+        cy.get('[data-cy="submissionBoxTitle"]', { timeout: TIMEOUT.EXTRA_LONG }).should('contain', submissionBoxTitle2)
         cy.get('[data-cy="submissionBoxDate"]', { timeout: TIMEOUT.EXTRA_LONG }).should('contain', submissionBoxTime.toDateString().slice(4))
         cy.get('[data-cy="submissionBoxDesc"]', { timeout: TIMEOUT.EXTRA_LONG }).should('contain', submissionBoxDescription)
 
         cy.get('[data-cy="edit-icon"]').should('be.visible').click()
         // Determine if title values are correct and change values
         cy.get('[data-cy="submissionBoxTitleEditing"]').should('be.visible').click()
-        cy.get('[data-cy="submissionBoxTitleEditing"]').focused().should('have.value', fullSubmissionBoxTitle)
+        cy.get('[data-cy="submissionBoxTitleEditing"]').focused().should('have.value', submissionBoxTitle2)
         cy.get('[data-cy="submissionBoxTitleEditing"]').focused().clear().type(newTitle).should('have.value', newTitle)
         // Determine if description values are correct and change values
         cy.get('[data-cy="submissionBoxDescEditing"]').should('be.visible').click()
@@ -83,7 +81,7 @@ describe('Recieving Dashboard Details Page Tests', () => {
         // Determine if date values are correct and change values
         cy.get('.data-cy-date-time-picker').should('be.visible').click()
         cy.get('.data-cy-date-time-picker').focused().should('have.value', submissionBoxTimeString)
-        cy.get('.data-cy-date-time-picker').focused().clear().type(newTimeCharacters).should('have.value', newTimeDisplay)
+        cy.get('.data-cy-date-time-picker').focused().clear().type(newTimeCharacters.replaceAll(' ', '')).should('have.value', newTimeDisplay)
         cy.get('[data-cy="updateButton"]').should('be.visible').click()
 
         cy.get('[data-cy="submissionBoxTitle"]').should('contain', newTitle)
@@ -97,11 +95,11 @@ describe('Recieving Dashboard Details Page Tests', () => {
         cy.wait(1000)
         cy.get('[data-cy="Manage Boxes"]').click()
 
-        cy.get(`[data-cy="${ fullSubmissionBoxTitle }"]`)
+        cy.get(`[data-cy="${ submissionBoxTitle2 }"]`)
         cy.wait(1000)   // Waiting for it to be clickable
-        cy.get(`[data-cy="${ fullSubmissionBoxTitle }"]`).click()
+        cy.get(`[data-cy="${ submissionBoxTitle2 }"]`).click()
 
-        cy.get('[data-cy="submissionBoxTitle"]', { timeout: TIMEOUT.EXTRA_LONG }).should('contain', fullSubmissionBoxTitle)
+        cy.get('[data-cy="submissionBoxTitle"]', { timeout: TIMEOUT.EXTRA_LONG }).should('contain', submissionBoxTitle2)
         cy.get('[data-cy="submissionBoxDate"]', { timeout: TIMEOUT.EXTRA_LONG }).should('contain', submissionBoxTime.toDateString().slice(4))
         cy.get('[data-cy="submissionBoxDesc"]', { timeout: TIMEOUT.EXTRA_LONG }).should('contain', submissionBoxDescription)
 
@@ -114,23 +112,25 @@ describe('Recieving Dashboard Details Page Tests', () => {
         cy.get('[data-cy="submissionBoxDescEditing"]').focused().clear().type(newDescription).should('have.value', newDescription)
         // Change time value
         cy.get('.data-cy-date-time-picker').should('be.visible').click()
-        cy.get('.data-cy-date-time-picker').focused().clear().type(newTimeCharacters).should('have.value', newTimeDisplay)
+        cy.get('.data-cy-date-time-picker').focused().clear().type(newTimeCharacters.replaceAll(' ', '')).should('have.value', newTimeDisplay)
         // Discard all changes
         cy.get('[data-cy="cancelButton"]').should('be.visible').click()
         // Values should revert to old values
-        cy.get('[data-cy="submissionBoxTitle"]').should('contain', fullSubmissionBoxTitle)
+        cy.get('[data-cy="submissionBoxTitle"]').should('contain', submissionBoxTitle2)
         cy.get('[data-cy="submissionBoxDesc"]').should('contain', submissionBoxDescription)
         cy.get('[data-cy="submissionBoxDate"]').should('contain', submissionBoxTime.toDateString().slice(4))
     })
 
     it('should not allow the form to submit with no title or and invalid date is entered', () => {
+        const invalidTimeCharacters = '1995042312:00PM'
+
         cy.get('[data-cy="Manage Boxes"]')
         cy.wait(1000)
         cy.get('[data-cy="Manage Boxes"]').click()
 
-        cy.get(`[data-cy="${ emptySubmissionBoxTitle }"]`)
+        cy.get(`[data-cy="${ submissionBoxTitle1 }"]`)
         cy.wait(1000)   // Waiting for it to be clickable
-        cy.get(`[data-cy="${ emptySubmissionBoxTitle }"]`).click()
+        cy.get(`[data-cy="${ submissionBoxTitle1 }"]`).click()
 
         // Start the editing process
         cy.get('[data-cy="edit-icon"]').should('be.visible').click()
