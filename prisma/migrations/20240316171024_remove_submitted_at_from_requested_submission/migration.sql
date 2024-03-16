9c79a451def499ea66acef45a36b79c42722a419
te-sa <teresa_saller@freenet.de>
2024-03-16 10:10:43 -0700

    Removed submittedAt from RequestedSubmission since we store videoVersions with submittedAt

---
+++ prisma/migrations/20240316171024_remove_submitted_at_from_requested_submission/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `submittedAt` on the `RequestedSubmission` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "RequestedSubmission" DROP COLUMN "submittedAt";
