3f2991e48464f8d105e189452189738c57982421
te-sa <teresa_saller@freenet.de>
2024-03-27 10:02:44 -0700

    Added tests to existing files

--- cypress/e2e/app/submission-box/detail/myBoxesDetails.cy.ts
+++ cypress/e2e/app/submission-box/detail/myBoxesDetails.cy.ts
@@ -6,6 +6,7 @@ describe('Receiving Dashboard Details Page Tests', () => {
     const fakeEmail = 'invalidUser@mail.com'
     const moreFakeEmail = 'wowsuchuser@person.com'
     const password = 'Pass1234'
+
     beforeEach(() => {
         cy.task('clearDB')
         // Can create the same user for each test, but need to create two separate submission boxes
@@ -240,5 +241,54 @@ describe('Receiving Dashboard Details Page Tests', () => {
         cy.wait(1000)
         cy.get('[data-cy="back-button"]').should('exist').and('be.visible').click()
         cy.url({ timeout: TIMEOUT.EXTRA_LONG }).should('contain', 'submission-box')
+    })
+
+    // Submission Box information
+    const submissionBoxTitle1 = 'Test Viewing With Members'
+    const submissionBoxTitle2 = 'Test Viewing Without Members'
+    const submissionBoxDescription = 'Description'
+    const submissionBoxTime = new Date('December 17, 2045 03:24:00')
+    const invitedEmail = 'invited@member.email'
+
+    it('Should allow the user to view members if there are members', () => {
+        cy.task('getUserId', email).then((userId) => {
+            cy.task('createSubmissionBoxWithEmail', { submissionBoxTitle: submissionBoxTitle1, email: invitedEmail, userId, submissionBoxDescription})
+            cy.task('createSubmissionBoxForSubmissions', { submissionBoxTitle: submissionBoxTitle2, submissionBoxDescription, userId, closesAt: submissionBoxTime })
+        })
+        cy.reload()
+        cy.visit('/dashboard')
+
+        cy.get('[data-cy="Manage Boxes"]')
+        cy.wait(1000)
+        cy.get('[data-cy="Manage Boxes"]').click()
+
+        cy.get(`[data-cy="${ submissionBoxTitle1 }"]`)
+        cy.wait(1000)   // Waiting for it to be clickable
+        cy.get(`[data-cy="${ submissionBoxTitle1 }"]`).click()
+
+        cy.get('[data-cy="submissionBoxMembersHeading"]').should('be.visible')
+
+        cy.get('[data-cy="submissionBoxMembers"]').should('contain', invitedEmail)
+    })
+
+    it('Should not display members if there aren\'t any', () => {
+        cy.task('getUserId', email).then((userId) => {
+            cy.task('createSubmissionBoxWithEmail', { submissionBoxTitle: submissionBoxTitle1, email: invitedEmail, userId, submissionBoxDescription})
+            cy.task('createSubmissionBoxForSubmissions', { submissionBoxTitle: submissionBoxTitle2, submissionBoxDescription, userId, closesAt: submissionBoxTime })
+        })
+        cy.reload()
+        cy.visit('/dashboard')
+
+        cy.get('[data-cy="Manage Boxes"]')
+        cy.wait(1000)
+        cy.get('[data-cy="Manage Boxes"]').click()
+
+        cy.get(`[data-cy="${ submissionBoxTitle2 }"]`)
+        cy.wait(1000)   // Waiting for it to be clickable
+        cy.get(`[data-cy="${ submissionBoxTitle2 }"]`).click()
+
+        cy.get('[data-cy="submissionBoxMembersHeading"]').should('not.exist')
+
+        cy.get('[data-cy="submissionBoxMembers"]').should('not.exist')
     })
 })
