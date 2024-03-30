c510e908f0e0fdcb36e21173b22db5951e9f5b57
justino599 <justin.schoenit@gmail.com>
2024-03-30 12:11:52 -0700

    Implemented functionality of requesting new submissions on frontend. Also confirm uninviting member.

--- src/components/SubmissionBoxDetails/index.tsx
+++ src/components/SubmissionBoxDetails/index.tsx
@@ -3,10 +3,12 @@
 import Typography from '@mui/material/Typography'
 import React, { useState } from 'react'
 import { RequestedSubmission, SubmissionBox } from '@prisma/client'
-import { Box, Chip } from '@mui/material'
+import { Box, Chip, Dialog, DialogActions, DialogTitle, Modal } from '@mui/material'
 import Button from '@mui/material/Button'
 import { AddCircleOutline } from '@mui/icons-material'
 import { toast } from 'react-toastify'
+import { theme } from '@/components/ThemeRegistry/theme'
+import SubmissionBoxRequestSubmission from '@/components/SubmissionBoxRequestSubmission'
 
 export type SubmissionBoxInfoProps = {
     submissionBox: SubmissionBox & { requestedSubmissions: RequestedSubmission[]} | null
@@ -15,6 +17,9 @@ export type SubmissionBoxInfoProps = {
 }
 export default function SubmissionBoxDetails(props: SubmissionBoxInfoProps) {
     const [members, setMembers] = useState(props.submissionBox?.requestedSubmissions.map(requestedSubmission => requestedSubmission.email) ?? [])
+    const [proposedDeleteMember, setProposedDeleteMember] = useState<string | null>(null)
+    const [inviteMembersModalOpen, setInviteMembersModalOpen] = useState(false)
+    const [addedEmails, setAddedEmails] = useState<string[]>([])
 
     function handleMemberDelete(deletedMember: string) {
         const newMembers = members.filter(member => member !== deletedMember)
@@ -31,6 +36,33 @@ export default function SubmissionBoxDetails(props: SubmissionBoxInfoProps) {
                 setMembers([...newMembers, deletedMember])
             }
         })
+    }
+
+    function clearAddMembersModal() {
+        setAddedEmails([])
+        setInviteMembersModalOpen(false)
+    }
+
+    function inviteMembers() {
+        const newMembers = addedEmails.filter(member => !members.includes(member))
+        if (addedEmails.length > 0) {
+            fetch('/api/submission-box/invite', {
+                method: 'POST',
+                body: JSON.stringify({
+                    submissionBoxId: props.submissionBox?.id,
+                    emails: newMembers,
+                }),
+            }).then(res => {
+                if (!res.ok) {
+                    toast.error('Failed to invite entered emails')
+                    setMembers(members.filter(member => !newMembers.includes(member)))
+                } else {
+                    toast.success('Members Successfully Invited!')
+                }
+            })
+        }
+        setMembers([...members, ...newMembers])
+        clearAddMembersModal()
     }
 
     return (
@@ -83,17 +115,110 @@ export default function SubmissionBoxDetails(props: SubmissionBoxInfoProps) {
                     </Typography>
                 </Box>
             )}
-            <Box>
-                <Typography data-cy='submissionBoxMembersHeading' color={'textSecondary'} sx={{ fontWeight: 'bold' }}>
-                    Members
-                </Typography>
-                <Box data-cy='submissionBoxMembers'>
-                    {members.map(email =>
-                        <Chip sx={{ m: 0.5, ml: 0 }} key={`submission-box-chip-${ email }`} label={email} onDelete={() => handleMemberDelete(email)}/>
-                    )}
-                    <Chip variant='outlined' label='Add Members' icon={<AddCircleOutline/>} onClick={() => console.log('boop')}/>
-                </Box>
-            </Box>
+            {props.isOwned && (
+                <>
+                    <Box>
+                        <Typography data-cy='submissionBoxMembersHeading' color={'textSecondary'} sx={{ fontWeight: 'bold' }}>
+                            Members
+                        </Typography>
+                        <Box data-cy='submissionBoxMembers'>
+                            {members.map(email =>
+                                <Chip sx={{ m: 0.5, ml: 0 }} key={`submission-box-chip-${ email }`} label={email} onDelete={() => setProposedDeleteMember(email)}/>
+                            )}
+                            <Chip variant='outlined' label='Add Members' icon={<AddCircleOutline fontSize='small'/>} onClick={() => setInviteMembersModalOpen(true)}/>
+                        </Box>
+                    </Box>
+                    <Dialog
+                        open={!!proposedDeleteMember}
+                        onClose={() => setProposedDeleteMember(null)}
+                    >
+                        <DialogTitle>Are you sure you want to uninvite {proposedDeleteMember} from this submission box?</DialogTitle>
+                        <DialogActions
+                            sx={{
+                                p: 2,
+                            }}
+                        >
+                            <Button onClick={
+                                () => {
+                                    setProposedDeleteMember(null)
+                                }
+                            }>No</Button>
+                            <Button
+                                onClick={
+                                    () => {
+                                        proposedDeleteMember && handleMemberDelete(proposedDeleteMember)
+                                        setProposedDeleteMember(null)
+                                    }
+                                }
+                                variant='contained'
+                                autoFocus
+                            >Yes</Button>
+                        </DialogActions>
+                    </Dialog>
+                    <Modal
+                        open={inviteMembersModalOpen}
+                        onClose={clearAddMembersModal}
+                    >
+                        <Box sx={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            position: 'absolute' as 'absolute',
+                            top: '50%',
+                            left: '50%',
+                            transform: 'translate(-50%, -50%)',
+                            width: '70vw',
+                            height: '80vh',
+                            minHeight: '19rem',
+                            minWidth: '29rem',
+                            backgroundColor: theme.palette.background.default,
+                            boxShadow: 24,
+                            borderRadius: '1rem',
+                            pb: 3,
+                        }}>
+                            <Box
+                                sx={{
+                                    display: 'flex',
+                                    flexDirection: 'row',
+                                    justifyContent: 'space-between',
+                                    alignItems: 'start',
+                                }}
+                            >
+                                <Typography
+                                    variant='h4'
+                                    sx={{
+                                        pl: 4,
+                                        pt: 3,
+                                        fontWeight: 500,
+                                        color: 'text.secondary',
+                                    }}
+                                >Request Submissions</Typography>
+                            </Box>
+                            <Box
+                                sx={{
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    alignItems: 'center',
+                                    width: '100%',
+                                    height: '100%',
+                                }}
+                            >
+                                <SubmissionBoxRequestSubmission emails={addedEmails} setEmailFieldText={() => {}} setEmails={setAddedEmails}/>
+                            </Box>
+                            <Box
+                                sx={{
+                                    display: 'flex',
+                                    gap: '1rem',
+                                    justifyContent: 'end',
+                                    mx: '1rem',
+                                }}
+                            >
+                                <Button onClick={clearAddMembersModal}>Cancel</Button>
+                                <Button variant='contained' onClick={inviteMembers}>Invite All</Button>
+                            </Box>
+                        </Box>
+                    </Modal>
+                </>
+            )}
             {props.onUnsubmit && (
                 <Box
                     sx={{
