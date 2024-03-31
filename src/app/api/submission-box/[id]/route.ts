e4a071a40028b5b7708010975bcc38967f2a6b35
te-sa <teresa_saller@freenet.de>
2024-03-30 21:44:19 -0700

    Returning email conditionally

--- src/app/api/submission-box/[id]/route.ts
+++ src/app/api/submission-box/[id]/route.ts
@@ -137,7 +137,7 @@ export async function GET(req: NextRequest): Promise<NextResponse> {
                         },
                         take: 1,
                     },
-                    email: true,
+                    email: doesUserOwnSubmissionBox,
                 },
             })
 
