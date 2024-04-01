93e305d8a1a6f82fbca898535a7536df26c406e0
Erin Hiebert <eehieb10@telus.net>
2024-04-01 11:30:24 -0700

    Addressed changes

--- cypress/e2e/app/dashboard/requestedBadge.cy.ts
+++ cypress/e2e/app/dashboard/requestedBadge.cy.ts
@@ -45,4 +45,74 @@ describe('Dashboard requested submission badge count tests', () => {
             })
         })
     })
+
+    it('should increment the count after users remove the submission from a requested submission', () => {
+        const submissionBoxTitle = 'Badge testing simulator'
+        const videoTitle = 'Get rid of the badge'
+        cy.task('getUserId', email).then((userId) => {
+            cy.task('createRequestSubmissionForUser', {userId, submissionBoxTitle}).then((requestedSubmissionId) => {
+                cy.reload()
+                // wait for api fetch
+                cy.wait(1000)
+                cy.get('[data-cy=requested-badge]').should('be.visible').should('contain', '1')
+                cy.task('createOneVideoAndRetrieveVideoId', {
+                    title: videoTitle,
+                    ownerId: userId,
+                }).then((videoId) => {
+                    cy.task('submitVideoToSubmissionBox', {
+                        requestedSubmissionId,
+                        videoId,
+                    }).then(() => {
+                        cy.reload()
+                        cy.visit('/dashboard?tab=my-invitations')
+                        cy.get('[data-cy="My Invitations"]')
+                        cy.wait(1000)
+                        cy.get('[data-cy="My Invitations"]').click()
+                        cy.get(`[data-cy="${ submissionBoxTitle }"]`)
+                        cy.wait(1000)
+                        cy.get(`[data-cy="${ submissionBoxTitle }"]`).click()
+                        cy.wait(1000)
+                        cy.get('[data-cy="unsubmit-button"]').should('be.visible').click()
+                        cy.get('button').last().should('be.visible').and('have.text', 'Yes').click()
+
+                        cy.visit('/dashboard')
+                        cy.get('[data-cy=requested-badge]').should('be.visible').should('contain', '1')
+                    })
+                })
+            })
+        })
+    })
+
+    it('should increment the count after users deletes the video submission of a requested submission', () => {
+        const submissionBoxTitle = 'Badge testing simulator'
+        const videoTitle = 'Get rid of the badge'
+        cy.task('getUserId', email).then((userId) => {
+            cy.task('createRequestSubmissionForUser', {userId, submissionBoxTitle}).then((requestedSubmissionId) => {
+                cy.reload()
+                // wait for api fetch
+                cy.wait(1000)
+                cy.get('[data-cy=requested-badge]').should('be.visible').should('contain', '1')
+                cy.task('createOneVideoAndRetrieveVideoId', {
+                    title: videoTitle,
+                    ownerId: userId,
+                }).then((videoId) => {
+                    cy.task('submitVideoToSubmissionBox', {
+                        requestedSubmissionId,
+                        videoId,
+                    }).then(() => {
+                        cy.reload()
+                        cy.wait(1000)
+                        cy.get('[data-cy="video-list"]').children().first().should('contain', videoTitle).click()
+                        cy.wait(1000)
+                        cy.get('[data-cy="edit-icon"]').click()
+                        cy.get('[data-cy="detail-video-delete-button"]').click()
+                        cy.get('[data-cy="detail-video-delete-confirm-button"]').click()
+
+                        cy.visit('/dashboard')
+                        cy.get('[data-cy=requested-badge]').should('be.visible').should('contain', '1')
+                    })
+                })
+            })
+        })
+    })
 })
--- src/app/api/submission-box/requestedsubmissions/require-submission/route.ts
+++ src/app/api/submission-box/requestedsubmissions/require-submission/route.ts
@@ -4,12 +4,14 @@ import logger from '@/utils/logger'
 import prisma from '@/lib/prisma'
 
 export async function GET() {
+    // This API will return the count of requested submissions that the user has not yet submitted to, call with GET
     try {
+        // Check if a user is logged in
         const session = await getServerSession()
         if (!session || !session.user?.email) {
             return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
         }
-
+        // Get userId
         const userId: string = (
             await prisma.user.findUniqueOrThrow({
                 where: {
@@ -20,7 +22,7 @@ export async function GET() {
                 },
             })
         ).id
-
+        // This query returns the count of requested submissions with a videoVersions of none
         const requiredCount = await prisma.requestedSubmission.aggregate({
             where: {
                 userId: userId,
@@ -30,7 +32,6 @@ export async function GET() {
                 id: true,
             },
         })
-
 
         return NextResponse.json({ requiredCount: requiredCount._count.id }, { status: 200 })
     } catch (error) {
